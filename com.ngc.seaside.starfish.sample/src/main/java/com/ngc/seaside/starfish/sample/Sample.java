04abfcc607b3d83b26c7707cd987e53a606e3a79
Tyler King <Tyler.King@ngc.com>
2017-04-11 16:02:34 -0500

    Correct format of project

--- com.ngc.seaside.starfish.sample/src/main/java/com/ngc/seaside/starfish/sample/Sample.java
+++ com.ngc.seaside.starfish.sample/src/main/java/com/ngc/seaside/starfish/sample/Sample.java
@@ -1,14 +1,17 @@
 package com.ngc.seaside.starfish.sample;
 
-public class Sample {
-	
-	public static void main(String[] args) {
-		Sample sample = new Sample();
-		System.out.println(sample.getHelloWorld());
-	}
-	
-	public String getHelloWorld() {
-		return "Hello World";
-	}
-	
+public class Sample
+{
+
+   public static void main(String[] args)
+   {
+      Sample sample = new Sample();
+      System.out.println(sample.getHelloWorld());
+   }
+
+   public String getHelloWorld()
+   {
+      return "Hello World";
+   }
+
 }
--- com.ngc.seaside.starfish.sample/src/test/java/com/ngc/seaside/starfish/sample/SampleTest.java
+++ com.ngc.seaside.starfish.sample/src/test/java/com/ngc/seaside/starfish/sample/SampleTest.java
@@ -4,18 +4,21 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class SampleTest {
-	
-	private Sample sample;
-	
-	@Before
-	public void before() {
-		sample = new Sample();
-	}
-	
-	@Test
-	public void test() {
-		Assert.assertEquals("Hello World", sample.getHelloWorld());
-	}
-	
+public class SampleTest
+{
+
+   private Sample sample;
+
+   @Before
+   public void before()
+   {
+      sample = new Sample();
+   }
+
+   @Test
+   public void test()
+   {
+      Assert.assertEquals("Hello World", sample.getHelloWorld());
+   }
+
 }
