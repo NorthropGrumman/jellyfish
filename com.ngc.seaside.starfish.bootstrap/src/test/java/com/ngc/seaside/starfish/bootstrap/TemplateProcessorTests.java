74c7c9ebb7bcd65742bb1595975d8d5c5061b480
perkinsb <Blake.Perkins@ngc.com>
2017-04-25 14:31:13 -0500

    Added unit tests for the TemplateProcessor class

--- com.ngc.seaside.starfish.bootstrap/src/test/java/com/ngc/seaside/starfish/bootstrap/TemplateProcessorTests.java
+++ com.ngc.seaside.starfish.bootstrap/src/test/java/com/ngc/seaside/starfish/bootstrap/TemplateProcessorTests.java
@@ -1,16 +1,200 @@
 package com.ngc.seaside.starfish.bootstrap;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class TemplateProcessorTests
 {
+   private Path zipFolder;
+   private int fileCount;
+   private Path templatePath;
+
+   @Before
+   public void setup() throws URISyntaxException
+   {
+      zipFolder = Paths.get(getClass().getClassLoader().getResource("testZip.zip").toURI());
+      templatePath = Paths.get(getClass().getClassLoader().getResource("TemplateExample").toURI());
+      fileCount = 0;
+   }
 
    @Test
    public void testUnzip()
    {
-      assertTrue(true);
+      Path results;
+
+      /*
+       * Test 1: Valid zip folder
+       */
+
+      // Setup expected
+      Set<String> expectedFiles = new HashSet<>();
+      expectedFiles.add(".gitignore");
+      expectedFiles.add("build.gradle");
+      expectedFiles.add("Sample.java");
+      expectedFiles.add("SampleTest.java");
+      expectedFiles.add("template.properties");
+
+      int expectedFileCount = 5;
+
+      // Method to test
+      try {
+         results = TemplateProcessor.unzip(zipFolder);
+
+         assertNotNull(results);
+
+         Files.walkFileTree(results, new SimpleFileVisitor<Path>()
+         {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException
+            {
+               assertTrue(expectedFiles.contains(file.getFileName().toString()));
+               fileCount++;
+               return FileVisitResult.CONTINUE;
+            }
+         });
+
+         assertEquals(expectedFileCount, fileCount);
+         TemplateGenerator.deleteRecursive(results, false);
+      }
+      catch (IOException e) {
+         fail("Error processing file");
+      }
+
+      /*
+       * Test 2: Invalid zip folder
+       */
+      results = null;
+
+      try {
+         results = TemplateProcessor.unzip(templatePath);
+      }
+      catch (IOException e) {
+         // This exception is expected
+         return;
+      }
    }
 
+   @Test
+   public void testValidateTemplate() throws URISyntaxException
+   {
+      /*
+       * Test 1: Valid template
+       */
+      try {
+         TemplateProcessor.validateTemplate(templatePath);
+      }
+      catch (ExitException e) {
+         fail();
+      }
+
+      /*
+       * Test 2: Invalid template: template.properties not found
+       */
+
+      // Setup
+      templatePath = Paths.get(getClass().getClassLoader().getResource("InvalidTemplates").toURI()).resolve("NoProperties");
+
+      try {
+         TemplateProcessor.validateTemplate(templatePath);
+      }
+      catch (ExitException e) {
+         assertEquals("Invalid template: template.properties not found", e.getMessage());
+      }
+
+      /*
+       * Test 3: Invalid template: template.properties must be a file
+       */
+
+      // Setup
+      templatePath = Paths.get(getClass().getClassLoader().getResource("InvalidTemplates").toURI()).resolve("NotAFile");
+
+      try {
+         TemplateProcessor.validateTemplate(templatePath);
+      }
+      catch (ExitException e) {
+         assertEquals("Invalid template: template.properties must be a file", e.getMessage());
+      }
+
+      /*
+       * Test 4: Invalid template: template folder not found
+       */
+
+      // Setup
+      templatePath = Paths.get(getClass().getClassLoader().getResource("InvalidTemplates").toURI()).resolve("NoFolderFound");
+
+      try {
+         TemplateProcessor.validateTemplate(templatePath);
+      }
+      catch (ExitException e) {
+         assertEquals("Invalid template: template folder not found", e.getMessage());
+      }
+
+      /*
+       * Test 5: Invalid template: template must be a folder
+       */
+
+      // Setup
+      templatePath = Paths.get(getClass().getClassLoader().getResource("InvalidTemplates").toURI()).resolve("NotAFolder");
+
+      try {
+         TemplateProcessor.validateTemplate(templatePath);
+      }
+      catch (ExitException e) {
+         assertEquals("Invalid template: template must be a folder", e.getMessage());
+      }
+   }
+
+   @Test
+   public void testParseTemplateProperties() throws URISyntaxException
+   {
+      LinkedHashMap<String, String> actual;
+      LinkedHashMap<String, String> expected;
+
+      /**
+       * Test 1: Verify properties are placed in the hashmap
+       */
+
+      // Setup
+      templatePath = Paths.get(getClass().getClassLoader().getResource("TemplateExample").toURI()).resolve("template.properties");
+      expected = new LinkedHashMap<>();
+
+      expected.put("groupId", "com.ngc.seaside.starfish");
+      expected.put("artifactId", "sample");
+      expected.put("version", "1.0-SNAPSHOT");
+
+      // Method to test
+      try {
+         actual = TemplateProcessor.parseTemplateProperties(templatePath);
+         assertEquals(expected.size(), actual.size());
+
+         for (Entry<String, String> entry : actual.entrySet()) {
+
+            if (!expected.containsKey(entry.getKey())) {
+               fail();
+            }
+            assertEquals(expected.get(entry.getKey()), entry.getValue());
+         }
+      }
+      catch (IOException e) {
+         fail();
+      }
+   }
 }
