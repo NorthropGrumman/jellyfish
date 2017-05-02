fbf075128c78865a410c528721dda47309260106
thooper <thomas.hooper@ngc.com>
2017-05-02 10:51:19 -0500

    Update CommandLine to use shorthand notation for template names. Command line will now default to look in $appHome/resources/config for template zip files.

--- com.ngc.seaside.starfish.bootstrap/src/main/java/com/ngc/seaside/starfish/bootstrap/CommandLine.java
+++ com.ngc.seaside.starfish.bootstrap/src/main/java/com/ngc/seaside/starfish/bootstrap/CommandLine.java
@@ -1,5 +1,7 @@
 package com.ngc.seaside.starfish.bootstrap;
 
+import org.apache.commons.lang.StringUtils;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -13,8 +15,9 @@ import java.util.function.Predicate;
 /**
  * Class for parsing command line arguments for this program and querying the user
  */
-public class CommandLine
-{
+public class CommandLine {
+   final static String APP_HOME_SYS_PROPERTY = "appHome";
+
    private Path templateFile;
    private Path outputFolder;
    private boolean clean;
@@ -23,50 +26,45 @@ public class CommandLine
    /**
     * Use {@link #parseArgs(String...)} for creating an instance
     */
-   private CommandLine()
-   {
+   private CommandLine() {
 
    }
 
    /**
     * Returns the folder of the unzipped template file.
-    * 
+    *
     * @return the folder of the unzipped template file
     */
-   public Path getTemplateFile()
-   {
+   public Path getTemplateFile() {
       return templateFile;
    }
 
    /**
     * Returns the output folder for generating the instance of the template.
-    * 
+    *
     * @return the output folder for generating the instance of the template
     */
-   public Path getOutputFolder()
-   {
+   public Path getOutputFolder() {
       return outputFolder;
    }
 
    /**
     * Returns whether or not to delete folders before creating them again.
-    * 
+    *
     * @return Whether or not to delete folders before creating them again
     */
-   public boolean isClean()
-   {
+   public boolean isClean() {
       return clean;
    }
 
    /**
     * Parses the arguments and returns a command line instance. Run with --help for help.
-    * 
+    *
     * @param args command line arguments
     * @return an instance of CommandLine
     * @throws ExitException if something occurred that would normally require the command line to exit
     */
-   public static CommandLine parseArgs(String... args)
-   {
+   public static CommandLine parseArgs(String... args) {
       CommandLine cl = new CommandLine();
       List<String> list = new ArrayList<>(Arrays.asList(args));
 
@@ -82,12 +80,10 @@ public class CommandLine
          if (outputFolderIndex + 1 < list.size()) {
             cl.outputFolder = Paths.get(list.get(outputFolderIndex + 1));
             list.subList(outputFolderIndex, outputFolderIndex + 2).clear();
-         }
-         else {
+         } else {
             throw new ExitException("Expected a folder after the option -o");
          }
-      }
-      else {
+      } else {
          cl.outputFolder = Paths.get(".").toAbsolutePath().normalize();
       }
 
@@ -96,8 +92,7 @@ public class CommandLine
       if (cleanIndex >= 0) {
          cl.clean = true;
          list.remove(cleanIndex);
-      }
-      else {
+      } else {
          cl.clean = false;
       }
 
@@ -112,21 +107,50 @@ public class CommandLine
       }
 
       // Template file processing
-      cl.templateFile = Paths.get(list.get(0));
-      if (!Files.exists(cl.templateFile)) {
-         throw new ExitException("File " + list.get(0) + " does not exist");
-      }
-      if (!Files.isRegularFile(cl.templateFile)) {
-         throw new ExitException("Expected a template file, not a directory: " + cl.templateFile.toAbsolutePath());
-      }
+      cl.templateFile = doGetTemplateFile(list);
       return cl;
+   }
+
+   /**
+    * Finds the path to the template file.  The first argument is treated as an absolute path.  If the file does not
+    * exists, the template is resolved by looking for ${appHome}/resources/config/${templateName}/Template${templateName}.zip.
+    *  If neither the absolute path or the appHome relative path points to an existing file an {@code ExitException} is
+    * generated.
+    *
+    * @param args the command line args
+    * @return the path to the template ZIP file (never {@code null})
+    */
+   private static Path doGetTemplateFile(List<String> args) {
+      String appHome = System.getProperty(APP_HOME_SYS_PROPERTY);
+      String templateName = args.get(0);
+      Path templateFile = Paths.get(templateName);
+      boolean fileFound = Files.exists(templateFile);
+
+      // If the file does not exists, check the default resources.
+      // Is the appHome system property set?
+      if (!fileFound && appHome != null && !appHome.trim().equals("")) {
+         // Try to find ${appHome}/resources/config/${templateName}/Template${templateName}.zip
+         templateFile = Paths.get(appHome,
+                                  "resources",
+                                  "config",
+                                  templateName.toLowerCase(), // path the directory lower case
+                                  "Template" + StringUtils.capitalize(templateName) + ".zip"); // make 1st char upper
+         fileFound = Files.exists(templateFile);
+      }
+
+      if (!fileFound) {
+         throw new ExitException("File " + args.get(0) + " does not exist");
+      }
+      if (!Files.isRegularFile(templateFile)) {
+         throw new ExitException("Expected a template file, not a directory: " + templateFile.toAbsolutePath());
+      }
+      return templateFile;
    }
 
    /**
     * Prints the usage description and options to the command line.
     */
-   private static void printHelp()
-   {
+   private static void printHelp() {
       System.out.println("usage: java -jar bootstrap.jar [-h] [-o output-file] [--clean] template-file");
       System.out.println();
       System.out.println("positional arguments:");
@@ -141,13 +165,12 @@ public class CommandLine
    /**
     * Queries the user to enter a value for the given parameter.
     *
-    * @param parameter name of the parameter
+    * @param parameter    name of the parameter
     * @param defaultValue default value for the parameter, or null
-    * @param validator function to determine whether a value is valid or not (can be null)
+    * @param validator    function to determine whether a value is valid or not (can be null)
     * @return a valid value for the parameter
     */
-   public static String queryUser(String parameter, String defaultValue, Predicate<String> validator)
-   {
+   public static String queryUser(String parameter, String defaultValue, Predicate<String> validator) {
       if (sc == null) {
          sc = new Scanner(System.in);
       }
@@ -160,8 +183,7 @@ public class CommandLine
          String line;
          try {
             line = sc.nextLine();
-         }
-         catch (NoSuchElementException e) {
+         } catch (NoSuchElementException e) {
             throw new ExitException("Unable to get value for parameter " + parameter);
          }
          if (line.isEmpty() && defaultValue != null) {
@@ -169,8 +191,7 @@ public class CommandLine
          }
          if (validator.test(line)) {
             return line;
-         }
-         else {
+         } else {
             System.out.println("Invalid value, please try again");
          }
       }
