163c6114e19827f3b9300fe3d6f745ffd4811471
thooper <thomas.hooper@ngc.com>
2017-05-24 13:19:37 -0500

    Add empty metadata singleton.

--- com.ngc.seaside.systemdescriptor.model.api/src/main/java/com/ngc/seaside/systemdescriptor/model/api/metadata/IMetadata.java
+++ com.ngc.seaside.systemdescriptor.model.api/src/main/java/com/ngc/seaside/systemdescriptor/model/api/metadata/IMetadata.java
@@ -10,17 +10,33 @@ import javax.json.JsonObject;
 public interface IMetadata {
 
   /**
+   * An immutable singleton that is used to indicate empty or missing metadata.
+   */
+  IMetadata EMPTY_METADATA = new IMetadata() {
+    @Override
+    public JsonObject getJson() {
+      return JsonObject.EMPTY_JSON_OBJECT;
+    }
+
+    @Override
+    public IMetadata setJson(JsonObject json) {
+      throw new UnsupportedOperationException("this object may not be modified!");
+    }
+  };
+
+  /**
    * Gets the JSON object that is the metadata.
    *
-   * @return the JSON object that is the metadata
+   * @return the JSON object that is the metadata (never {@code null})
    */
   JsonObject getJson();
 
   /**
-   * Sets the JSON object that is the metadata.
+   * Sets the JSON object that is the metadata.  Use {@link #EMPTY_METADATA} to set empty metadata on an object.
    *
    * @param json the JSON object that is the metadata
    * @return the JSON object that is the metadata
+   * @throws NullPointerException if {@code json} is {@code null}
    */
   IMetadata setJson(JsonObject json);
 }
