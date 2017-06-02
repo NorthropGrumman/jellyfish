0b032589751dda831711aac6aa901c1be55fff1a
thooper <thomas.hooper@ngc.com>
2017-06-02 11:00:04 -0500

    Add documentation.

--- com.ngc.seaside.systemdescriptor.service.api/src/main/java/com/ngc/seaside/systemdescriptor/service/api/ISystemDescriptorService.java
+++ com.ngc.seaside.systemdescriptor.service.api/src/main/java/com/ngc/seaside/systemdescriptor/service/api/ISystemDescriptorService.java
@@ -5,12 +5,39 @@ import com.ngc.seaside.systemdescriptor.model.api.ISystemDescriptor;
 import java.nio.file.Path;
 import java.util.Collection;
 
+/**
+ * The top level service used to interface with a system descriptor.  This is the entry point for most system descriptor
+ * related operations.
+ */
 public interface ISystemDescriptorService {
 
-  IParsingResult parseProject(Path projectDirectory);
+   /**
+    * Parses a system descriptor project where all the {@code .sd} files are organized under {@code src/main/sd}.
+    * Always check the {@link IParsingResult#isSuccessful() result} of parsing to
+    * before inspecting the {@code ISystemDescriptor}.  If the parsing was not successful, the descriptor may be in a
+    * inconsistent state.
+    *
+    * @param projectDirectory a path to the root directory of the system descriptor project
+    * @return the result of parsing
+    */
+   IParsingResult parseProject(Path projectDirectory);
 
-  IParsingResult parseFiles(Collection<Path> paths);
+   /**
+    * Parses the given {@code .sd} files.  Always check the {@link IParsingResult#isSuccessful() result} of parsing to
+    * before inspecting the {@code ISystemDescriptor}.  If the parsing was not successful, the descriptor may be in a
+    * inconsistent state.
+    *
+    * @param paths the paths that point to {@code .sd} files
+    * @return the result of parsing
+    */
+   IParsingResult parseFiles(Collection<Path> paths);
 
-  ISystemDescriptor immutableCopy(ISystemDescriptor descriptor);
+   /**
+    * Returns an immutable copy of the given {@code ISystemDescriptor}
+    *
+    * @param descriptor the descriptor to create a copy of
+    * @return an immutable copy
+    */
+   ISystemDescriptor immutableCopy(ISystemDescriptor descriptor);
 
 }
