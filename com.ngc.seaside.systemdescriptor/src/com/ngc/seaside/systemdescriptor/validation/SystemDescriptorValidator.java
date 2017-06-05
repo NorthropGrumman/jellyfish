3f1a78f019f233fdef21d6ba2c6f25d7fca265a9
thooper <thomas.hooper@ngc.com>
2017-06-05 08:14:49 -0500

    Add documentation.

--- com.ngc.seaside.systemdescriptor/src/com/ngc/seaside/systemdescriptor/validation/SystemDescriptorValidator.java
+++ com.ngc.seaside.systemdescriptor/src/com/ngc/seaside/systemdescriptor/validation/SystemDescriptorValidator.java
@@ -14,45 +14,62 @@ import com.ngc.seaside.systemdescriptor.extension.IValidatorExtension;
 /**
  * This is the main validator for the System Descriptor language. This validator
  * does not actually do any work; it delegates to more specific validators
- * declared in the {@code ComposedChecks} annotation as well as validator extensions
- * that have been registered.
+ * declared in the {@code ComposedChecks} annotation as well as validator
+ * extensions that have been registered.
  */
 @ComposedChecks(validators = {
 		DuplicateElementValidator.class,
 		ModelValidator.class,
 		ScenarioValidator.class,
-		LinkValidator.class}
-)
+		LinkValidator.class })
 public class SystemDescriptorValidator extends AbstractSystemDescriptorValidator
-	implements IValidatorExtension.ValidationHelper {
+		implements IValidatorExtension.ValidationHelper {
 
+	/**
+	 * Registered validator extensions.
+	 */
 	private final Collection<IValidatorExtension> validators = new ArrayList<>();
-	
+
 	@Check
 	public void invokeValidatorExtensions(EObject object) {
-		for(IValidatorExtension v : validators) {
+		for (IValidatorExtension v : validators) {
 			v.validate(object, this);
 		}
 	}
 
+	/**
+	 * Add a validator extension that will be invoke to perform validation as a
+	 * system descriptor is being parsed.
+	 * 
+	 * @param validator
+	 *            the validator to add
+	 */
 	public void addValidatorExtension(IValidatorExtension validator) {
 		validators.add(Preconditions.checkNotNull(validator, "validator may not be null!"));
 	}
-	
+
+	/**
+	 * Removes a validator extension.
+	 * 
+	 * @param validator
+	 *            the validator to remove
+	 * @return true if the validator was removed, false if the validator was not
+	 *         added
+	 */
 	public boolean removeValidatorExtension(IValidatorExtension validator) {
 		return validators.remove(Preconditions.checkNotNull(validator, "validator may not be null!"));
 	}
-	
+
 	@Override
 	public void error(String message, EObject source, EStructuralFeature feature) {
 		super.error(message, source, feature);
 	}
-	
+
 	@Override
 	public void warning(String message, EObject source, EStructuralFeature feature) {
 		super.warning(message, source, feature);
 	}
-	
+
 	@Override
 	public void info(String message, EObject source, EStructuralFeature feature) {
 		super.info(message, source, feature);
