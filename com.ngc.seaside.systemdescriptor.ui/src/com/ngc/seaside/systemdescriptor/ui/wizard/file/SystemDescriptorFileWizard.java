2053b2429bb7aa6c0daecd91e145f4717ed2b0b8
Tyler King <Tyler.King@ngc.com>
2017-06-06 15:11:50 -0500

    Create jellyfish dsl file wizard

---
+++ com.ngc.seaside.systemdescriptor.ui/src/com/ngc/seaside/systemdescriptor/ui/wizard/file/SystemDescriptorFileWizard.java
@@ -0,0 +1,80 @@
+package com.ngc.seaside.systemdescriptor.ui.wizard.file;
+
+import java.io.InputStream;
+import java.util.Map;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
+
+import com.ngc.seaside.systemdescriptor.ui.wizard.file.page.FilePage;
+
+public class SystemDescriptorFileWizard extends Wizard implements INewWizard
+{
+   private FilePage filePage;
+   private IPath defaultSourceFolder;
+   private String defaultPackage;
+
+   public SystemDescriptorFileWizard()
+   {
+      super();
+      setNeedsProgressMonitor(true);
+   }
+
+   @Override
+   public void addPages()
+   {
+      this.filePage = new FilePage(defaultSourceFolder, defaultPackage);
+      addPage(this.filePage);
+   }
+
+   @Override
+   public boolean performFinish()
+   {
+      IFile file = this.filePage.getAbsolutePath();
+
+      String filename = this.filePage.getFileName();
+      String packageName = this.filePage.getPackageName();
+      if (filename.toLowerCase().endsWith(".sd")) {
+         filename = filename.substring(filename.length() - 3);
+      }
+      boolean model = this.filePage.isModelSelected();
+
+      InputStream stream = SystemDescriptorFileSupport.createSDStream(packageName, filename, model);
+      try {
+         boolean success = SystemDescriptorFileSupport.createFile(getShell(), file, stream);
+         if (success) {
+            getShell().getDisplay().asyncExec(() -> {
+               IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+               try {
+                  IDE.openEditor(page, file, true);
+               }
+               catch (PartInitException localPartInitException) {
+               }
+            });
+         }
+         return success;
+      }
+      catch (CoreException e) {
+         e.printStackTrace();
+         return false;
+      }
+   }
+
+   @Override
+   public void init(IWorkbench workbench, IStructuredSelection selection)
+   {
+      Map.Entry<IPath, String> result = SystemDescriptorFileSupport.getDefaultSourceAndPackage(workbench, selection);
+      this.defaultSourceFolder = result.getKey();
+      this.defaultPackage = result.getValue();
+   }
+
+}
