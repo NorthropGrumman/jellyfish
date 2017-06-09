76ac837e5a541a3be298ea632577d97dfda1b23b
thooper <thomas.hooper@ngc.com>
2017-06-09 13:11:29 -0500

    Add documentation.

--- com.ngc.seaside.systemdescriptor.service.impl.xtext/src/main/java/com/ngc/seaside/systemdescriptor/service/impl/xtext/scenario/AbstractStepHandler.java
+++ com.ngc.seaside.systemdescriptor.service.impl.xtext/src/main/java/com/ngc/seaside/systemdescriptor/service/impl/xtext/scenario/AbstractStepHandler.java
@@ -3,7 +3,6 @@ package com.ngc.seaside.systemdescriptor.service.impl.xtext.scenario;
 import com.ngc.seaside.systemdescriptor.scenario.api.IScenarioStepHandler;
 import com.ngc.seaside.systemdescriptor.scenario.api.ScenarioStepVerb;
 import com.ngc.seaside.systemdescriptor.scenario.api.VerbTense;
-import com.ngc.seaside.systemdescriptor.validation.api.AbstractSystemDescriptorValidator;
 
 import java.util.Collections;
 import java.util.EnumMap;
