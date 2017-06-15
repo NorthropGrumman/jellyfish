9d6d82130307861d448614f1c870394d97eb16af
Tyler King <Tyler.King@ngc.com>
2017-06-15 09:28:10 -0500

    Update file and package wizard to expand package explorer when new resource is created

--- com.ngc.seaside.systemdescriptor.ui/src/com/ngc/seaside/systemdescriptor/ui/wizard/pkg/SystemDescriptorPackageWizard.java
+++ com.ngc.seaside.systemdescriptor.ui/src/com/ngc/seaside/systemdescriptor/ui/wizard/pkg/SystemDescriptorPackageWizard.java
@@ -6,9 +6,15 @@ import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IViewReference;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.ISetSelectionTarget;
 
 import com.ngc.seaside.systemdescriptor.ui.wizard.pkg.page.PackagePage;
 
@@ -42,6 +48,20 @@ public class SystemDescriptorPackageWizard extends Wizard implements INewWizard 
 		IFolder folder = this.packagePage.getAbsolutePath();
 		try {
 			SystemDescriptorPackageSupport.createFolder(getShell(), folder);
+			try {
+				IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+				for (IViewReference ref : page.getViewReferences()) {
+					IViewPart view = ref.getView(false);
+					if (view instanceof ISetSelectionTarget) {
+						getShell().getDisplay().asyncExec(() -> {
+							((ISetSelectionTarget) view).selectReveal(new StructuredSelection(folder));
+						});
+						
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace(System.err);
+			}
 		} catch (CoreException e) {
 			e.printStackTrace(System.err);
 			return false;
