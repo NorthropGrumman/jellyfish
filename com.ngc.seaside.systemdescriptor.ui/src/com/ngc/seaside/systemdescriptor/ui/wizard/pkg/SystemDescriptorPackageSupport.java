49b69fbade64124af9a364de54d32f18b271861b
Tyler King <Tyler.King@ngc.com>
2017-06-15 09:54:52 -0500

    Update package wizard to find default src folder similar to file wizard

--- com.ngc.seaside.systemdescriptor.ui/src/com/ngc/seaside/systemdescriptor/ui/wizard/pkg/SystemDescriptorPackageSupport.java
+++ com.ngc.seaside.systemdescriptor.ui/src/com/ngc/seaside/systemdescriptor/ui/wizard/pkg/SystemDescriptorPackageSupport.java
@@ -16,6 +16,12 @@ import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IPackageDeclaration;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IFileEditorInput;
@@ -33,29 +39,51 @@ public class SystemDescriptorPackageSupport {
 	 *            selected file in the package explorer
 	 * @return the default source folder and package
 	 */
-	static Map.Entry<IPath, String> getDefaultSourceAndPackage(IWorkbench workbench, IStructuredSelection selection) {
-		try {
-			Object selected = selection.getFirstElement();
+	static Map.Entry<IPath, String> getDefaultSourceAndPackage(IWorkbench workbench, IStructuredSelection selection)
+	   {
+	      try {
+	         Object selected = selection.getFirstElement();
 
-			if (selected instanceof IResource) {
-				return getDefaultSourceAndPackage(((IResource) selected).getProject().getLocation(),
-						((IResource) selected).getFullPath());
-			}
+	         if (selected instanceof IPackageFragment) {
+	            IPackageFragment pkg = (IPackageFragment) selected;
+	            return new AbstractMap.SimpleEntry<>(pkg.getParent().getPath(), pkg.getElementName());
+	         }
+	         if (selected instanceof IPackageFragmentRoot) {
+	            IPackageFragmentRoot pkgRoot = (IPackageFragmentRoot) selected;
+	            return new AbstractMap.SimpleEntry<>(pkgRoot.getPath(), "");
+	         }
+	         if (selected instanceof IJavaElement) {
+	            selected = ((IJavaElement) selected).getResource();
+	         }
+	         if (selected instanceof IResource) {
+	            return getDefaultSourceAndPackage(((IResource) selected).getProject().getLocation(), ((IResource) selected).getFullPath());
+	         }
+	         if (selected instanceof ICompilationUnit) {
+	            ICompilationUnit unit = (ICompilationUnit) selected;
+	            IPackageDeclaration[] pkg = null;
+	            try {
+	               pkg = unit.getPackageDeclarations();
+	               if (pkg.length > 0) {
+	                  return new AbstractMap.SimpleEntry<>(pkg[0].getParent().getPath(), pkg[0].getElementName());
+	               }
+	            }
+	            catch (JavaModelException e) {
+	            }
+	         }
 
-			// If all else fails look at the project of the active page in
-			// eclipse
+	         // If all else fails look at the project of the active page in eclipse
 
-			IFile file = ((IFileEditorInput) workbench.getActiveWorkbenchWindow().getActivePage().getActiveEditor()
-					.getEditorInput()).getFile();
-			Map.Entry<IPath, String> result = getDefaultSourceAndPackage(file.getProject().getLocation(),
-					file.getFullPath());
-			return new AbstractMap.SimpleEntry<>(result.getKey(), "");
-		} catch (NullPointerException e) {
-		} catch (Exception e) {
-			e.printStackTrace(System.err);
-		}
-		return new AbstractMap.SimpleEntry<>(new Path(""), "");
-	}
+	         IFile file = ((IFileEditorInput) workbench.getActiveWorkbenchWindow().getActivePage().getActiveEditor().getEditorInput()).getFile();
+	         Map.Entry<IPath, String> result = getDefaultSourceAndPackage(file.getProject().getLocation(), file.getFullPath());
+	         return new AbstractMap.SimpleEntry<>(result.getKey(), "");
+	      }
+	      catch (NullPointerException e) {
+	      }
+	      catch (Exception e) {
+	         e.printStackTrace(System.err);
+	      }
+	      return new AbstractMap.SimpleEntry<>(new Path(""), "");
+	   }
 
 	/**
 	 * Searches the path for a src folder and returns the default source folder
