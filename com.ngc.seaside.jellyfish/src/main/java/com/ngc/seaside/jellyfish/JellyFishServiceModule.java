99e553eca325e67a543682e3dedc97570c436d34
perkinsb <Blake.Perkins@ngc.com>
2017-06-22 09:26:16 -0500

    Fixed log4j issues

--- com.ngc.seaside.jellyfish/src/main/java/com/ngc/seaside/jellyfish/JellyFishServiceModule.java
+++ com.ngc.seaside.jellyfish/src/main/java/com/ngc/seaside/jellyfish/JellyFishServiceModule.java
@@ -28,33 +28,33 @@ public class JellyFishServiceModule extends AbstractModule {
       //The listener let's us know when the service has been bound
       //and then we call activate on the service.
       //TODO figure out how to deactivate the component and make this generic for all OSGi services
-//      bindListener(new AbstractMatcher<TypeLiteral>() {
-//         @Override
-//         public boolean matches(TypeLiteral literal) {
-//            return literal.getRawType().equals(LogService.class) ||
-//                     literal.getRawType().equals(ResourceService.class);
-//         }
-//      }, new TypeListener() {
-//         @Override
-//         public <I> void hear(TypeLiteral<I> type, TypeEncounter<I> encounter) {
-//            encounter.register((InjectionListener<I>) i -> {
-//               if(i instanceof LogService) {
-//                  logService = (LogService) i;
-//                  logService.activate();
-//                  if(resourceService != null) {
-//                     resourceService.setLogService(logService);
-//                     resourceService.activate();
-//                  }
-//               } else if(i instanceof ResourceService) {
-//                  resourceService = (ResourceService) i;
-//                  if(logService != null) {
-//                     resourceService.setLogService(logService);
-//                     resourceService.activate();
-//                  }
-//               }
-//            });
-//         }
-//      });
+      bindListener(new AbstractMatcher<TypeLiteral>() {
+         @Override
+         public boolean matches(TypeLiteral literal) {
+            return literal.getRawType().equals(LogService.class) ||
+                     literal.getRawType().equals(ResourceService.class);
+         }
+      }, new TypeListener() {
+         @Override
+         public <I> void hear(TypeLiteral<I> type, TypeEncounter<I> encounter) {
+            encounter.register((InjectionListener<I>) i -> {
+               if(i instanceof LogService) {
+                  logService = (LogService) i;
+                  logService.activate();
+                  if(resourceService != null) {
+                     resourceService.setLogService(logService);
+                     resourceService.activate();
+                  }
+               } else if(i instanceof ResourceService) {
+                  resourceService = (ResourceService) i;
+                  if(logService != null) {
+                     resourceService.setLogService(logService);
+                     resourceService.activate();
+                  }
+               }
+            });
+         }
+      });
 
       //bind the interface to the implementation
       bind(ILogService.class).to(LogService.class);
