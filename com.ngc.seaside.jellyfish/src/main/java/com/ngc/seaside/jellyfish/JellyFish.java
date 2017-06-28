3ea6ddc482d45dc03ba6aec75472949f38c84418
j11860 <j11860@LALFPCE00365942.northgrum.com>
2017-06-27 19:58:11 -0500

    Removed unnecessary problematic exports.

--- com.ngc.seaside.jellyfish/src/main/java/com/ngc/seaside/jellyfish/JellyFish.java
+++ com.ngc.seaside.jellyfish/src/main/java/com/ngc/seaside/jellyfish/JellyFish.java
@@ -13,19 +13,31 @@ import java.util.ServiceLoader;
 public class JellyFish {
 
    /**
-    * Run the JellyFish application
+    * Main to run the JellyFish application..
     * 
     * @param args the program arguments. The first argument should always be the name of the command in which to run
     *           followed by a list of parameters for that command.
     */
    public static void main(String[] args) {
-      Injector injector = getInjector();
-      JellyFishCommandProviderModule provider = injector.getInstance(JellyFishCommandProviderModule.class);
       try {
-         provider.run(args);
+    	  runCommandProvider(args);
       } catch (IllegalArgumentException e) {
          System.out.println("\nERROR: " + e.getMessage() + "\nTry running \"jellyfish help\" for usage information.");
       }
+   }
+
+   /**
+    * Run the JellyFish application
+    * 
+    * @param args the program arguments. The first argument should always be the name of the command in which to run
+    *           followed by a list of parameters for that command.
+    */
+   public static void runCommandProvider(String[] args)
+   {
+	   Injector injector = getInjector();
+	   JellyFishCommandProviderModule provider = injector.getInstance(JellyFishCommandProviderModule.class);
+	   
+	   provider.run(args);
    }
 
    /**
