43361b48d0dbda0707ea4b90036263e69426a8b3
Tyler King <Tyler.King@ngc.com>
2017-07-17 11:05:34 -0500

    Implement guice and osgi classes

--- com.ngc.seaside.jellyfish.cli.command.createjellyfishcommand/src/main/java/com/ngc/seaside/jellyfish/cli/command/createjellyfishcommand/CreateJellyFishCommandGuiceModule.java
+++ com.ngc.seaside.jellyfish.cli.command.createjellyfishcommand/src/main/java/com/ngc/seaside/jellyfish/cli/command/createjellyfishcommand/CreateJellyFishCommandGuiceModule.java
@@ -1,5 +1,16 @@
 package com.ngc.seaside.jellyfish.cli.command.createjellyfishcommand;
 
-public class CreateJellyFishCommandGuiceModule {
+import com.google.inject.AbstractModule;
+import com.google.inject.multibindings.Multibinder;
+import com.ngc.seaside.command.api.ICommand;
+import com.ngc.seaside.jellyfish.api.IJellyFishCommand;
 
+public class CreateJellyFishCommandGuiceModule extends AbstractModule {
+
+   @Override
+   protected void configure() {
+      Multibinder.newSetBinder(binder(), IJellyFishCommand.class).addBinding().to(CreateJellyFishCommandGuiceWrapper.class);
+
+      Multibinder.newSetBinder(binder(), ICommand.class).addBinding().to(CreateJellyFishCommandGuiceWrapper.class);
+   }
 }
