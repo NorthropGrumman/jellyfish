58fda416ff972a7255793a9e8b23a0798aeae0ef
perkinsb <Blake.Perkins@ngc.com>
2017-07-18 10:25:38 -0500

    Fixed formatting per review

--- com.ngc.seaside.bootstrap.impl.provider/src/main/java/com/ngc/seaside/bootstrap/impl/provider/BootstrapCommandProviderGuiceWrapper.java
+++ com.ngc.seaside.bootstrap.impl.provider/src/main/java/com/ngc/seaside/bootstrap/impl/provider/BootstrapCommandProviderGuiceWrapper.java
@@ -2,7 +2,6 @@ package com.ngc.seaside.bootstrap.impl.provider;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-
 import com.ngc.blocs.service.log.api.ILogService;
 import com.ngc.seaside.bootstrap.api.IBootstrapCommand;
 import com.ngc.seaside.bootstrap.api.IBootstrapCommandProvider;
@@ -17,41 +16,40 @@ import java.util.Set;
  */
 @Singleton
 public class BootstrapCommandProviderGuiceWrapper implements IBootstrapCommandProvider {
-	private final BootstrapCommandProvider delegate = new BootstrapCommandProvider();
+   private final BootstrapCommandProvider delegate = new BootstrapCommandProvider();
 
-	@Inject
-	public BootstrapCommandProviderGuiceWrapper(ILogService logService, ITemplateService templateService,
-			IParameterService parameterService, Set<IBootstrapCommand> commands) {
-		delegate.setLogService(logService);
-		delegate.setTemplateService(templateService);
-		delegate.setParameterService(parameterService);
-		commands.forEach(delegate::addCommand);
-		delegate.activate();
-	}
+   @Inject
+   public BootstrapCommandProviderGuiceWrapper(ILogService logService, ITemplateService templateService, IParameterService parameterService, Set<IBootstrapCommand> commands) {
+      delegate.setLogService(logService);
+      delegate.setTemplateService(templateService);
+      delegate.setParameterService(parameterService);
+      commands.forEach(delegate::addCommand);
+      delegate.activate();
+   }
 
-	@Override
-	public IUsage getUsage() {
-		return delegate.getUsage();
-	}
+   @Override
+   public IUsage getUsage() {
+      return delegate.getUsage();
+   }
 
-	@Override
-	public IBootstrapCommand getCommand(String commandName) {
-		return delegate.getCommand(commandName);
-	}
-	
-	@Override
-	public void addCommand(IBootstrapCommand command) {
-		delegate.addCommand(command);
-	}
+   @Override
+   public IBootstrapCommand getCommand(String commandName) {
+      return delegate.getCommand(commandName);
+   }
 
-	@Override
-	public void removeCommand(IBootstrapCommand command) {
-		delegate.removeCommand(command);
-	}
+   @Override
+   public void addCommand(IBootstrapCommand command) {
+      delegate.addCommand(command);
+   }
 
-	@Override
-	public void run(String[] arguments) {
-		delegate.run(arguments);
-	}
+   @Override
+   public void removeCommand(IBootstrapCommand command) {
+      delegate.removeCommand(command);
+   }
+
+   @Override
+   public void run(String[] arguments) {
+      delegate.run(arguments);
+   }
 
 }
