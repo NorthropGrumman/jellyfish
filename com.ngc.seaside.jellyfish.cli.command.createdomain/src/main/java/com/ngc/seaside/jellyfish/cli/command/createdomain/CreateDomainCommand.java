994546afdce011aa7989be1c6cf2ce3164146354
Tyler King <Tyler.King@ngc.com>
2017-07-19 11:13:31 -0500

    Create create-domain command usage

--- com.ngc.seaside.jellyfish.cli.command.createdomain/src/main/java/com/ngc/seaside/jellyfish/cli/command/createdomain/CreateDomainCommand.java
+++ com.ngc.seaside.jellyfish.cli.command.createdomain/src/main/java/com/ngc/seaside/jellyfish/cli/command/createdomain/CreateDomainCommand.java
@@ -22,6 +22,18 @@ public class CreateDomainCommand implements IJellyFishCommand {
 
    public static final String EXAMPLE_PROPERTY = "example";
 
+   public static final String GROUP_ID_PROPERTY = "groupId";
+   public static final String ARTIFACT_ID_PROPERTY = "artifactId";
+   public static final String VERSION_PROPERTY = "version";
+   public static final String PACKAGE_PROPERTY = "package";
+   public static final String PACKAGE_SUFFIX_PROPERTY = "packageSuffix";
+   public static final String OUTPUT_DIRECTORY_PROPERTY = "outputDirectory";
+   public static final String DOMAIN_TEMPLATE_FILE_PROPERTY = "domainTemplateFile";
+   public static final String MODEL_PROPERTY = "model";
+   public static final String USE_MODEL_STRUCTURE_PROPERTY = "useModelStructure";
+   public static final String IGNORE_STEREOTYPES_PROPERTY = "ignoreStereoTypes";
+   public static final String STEREOTYPES_PROPERTY = "stereoTypes";
+
    private ILogService logService;
 
    @Override
@@ -38,7 +50,7 @@ public class CreateDomainCommand implements IJellyFishCommand {
    public void run(IJellyFishCommandOptions commandOptions) {
       // TODO Auto-generated method stub
    }
-   
+
    @Activate
    public void activate() {
       logService.trace(getClass(), "Activated");
@@ -72,8 +84,39 @@ public class CreateDomainCommand implements IJellyFishCommand {
     * @return the usage.
     */
    private static IUsage createUsage() {
-      // TODO Auto-generated method stub
-      return new DefaultUsage("Description of create-domain command", new DefaultParameter(EXAMPLE_PROPERTY).setDescription("Description of example property").setRequired(false));
+      return new DefaultUsage("Generate a BLoCS domain model gradle project.",
+         new DefaultParameter(GROUP_ID_PROPERTY).setDescription("The project's group ID").setRequired(false),
+         new DefaultParameter(ARTIFACT_ID_PROPERTY).setDescription("The project's version").setRequired(false),
+         new DefaultParameter(VERSION_PROPERTY)
+                  .setDescription(
+                     "The project's artifactId (this argument may only be used if the model argument is set)")
+                  .setRequired(false),
+         new DefaultParameter(PACKAGE_PROPERTY)
+                  .setDescription(
+                     "The project's default package (this argument may only be used if the model argument is set)")
+                  .setRequired(false),
+         new DefaultParameter(PACKAGE_SUFFIX_PROPERTY)
+                  .setDescription("A string to append to the end of the generated package name").setRequired(false),
+         new DefaultParameter(OUTPUT_DIRECTORY_PROPERTY).setDescription("Base directory in which to output the file(s)")
+                  .setRequired(true),
+         new DefaultParameter(DOMAIN_TEMPLATE_FILE_PROPERTY).setDescription("The velocity template file")
+                  .setRequired(true),
+         new DefaultParameter(MODEL_PROPERTY)
+                  .setDescription(
+                     "The fully qualified path to the system descriptor model. If set it would only generate the input and output data for that model")
+                  .setRequired(false),
+         new DefaultParameter(USE_MODEL_STRUCTURE_PROPERTY)
+                  .setDescription(
+                     "If true, the system descriptor model's structural decomposition is used when outputting the domain model project")
+                  .setRequired(false),
+         new DefaultParameter(IGNORE_STEREOTYPES_PROPERTY)
+                  .setDescription(
+                     "Comma-separated string of the stereotypes to ignore when generating the domain model (e.g. \"virtual, system\")")
+                  .setRequired(false),
+         new DefaultParameter(STEREOTYPES_PROPERTY)
+                  .setDescription(
+                     "Comma-separated string of the stereotypes in which the domain should only be generated")
+                  .setRequired(false));
    }
 
 }
