4a9a1ba3adae76c677317aba4929b81a0f5c67d0
perkinsb <Blake.Perkins@ngc.com>
2017-07-21 15:07:06 -0500

    Committing tests for end of day.  Still broken

--- com.ngc.seaside.jellyfish.cli.command.createprotocolbuffermessages/src/test/java/com/ngc/seaside/jellyfish/cli/command/createprotocolbuffermessages/CreateProtocolbufferMessagesCommandTest.java
+++ com.ngc.seaside.jellyfish.cli.command.createprotocolbuffermessages/src/test/java/com/ngc/seaside/jellyfish/cli/command/createprotocolbuffermessages/CreateProtocolbufferMessagesCommandTest.java
@@ -7,35 +7,26 @@ import com.google.inject.Module;
 import com.ngc.blocs.guice.module.LogServiceModule;
 import com.ngc.blocs.service.log.api.ILogService;
 import com.ngc.blocs.test.impl.common.log.PrintStreamLogService;
-import com.ngc.seaside.command.api.DefaultParameter;
-import com.ngc.seaside.command.api.DefaultParameterCollection;
 import com.ngc.seaside.command.api.IParameter;
 import com.ngc.seaside.jellyfish.api.IJellyFishCommand;
 import com.ngc.seaside.jellyfish.api.IJellyFishCommandOptions;
-import com.ngc.seaside.systemdescriptor.model.api.ISystemDescriptor;
-import com.ngc.seaside.systemdescriptor.service.api.IParsingResult;
-import com.ngc.seaside.systemdescriptor.service.api.ISystemDescriptorService;
+import com.ngc.seaside.jellyfish.api.IJellyFishCommandProvider;
+import com.ngc.seaside.jellyfish.cli.command.createdomain.CreateDomainCommandGuiceWrapper;
 import com.ngc.seaside.systemdescriptor.service.impl.xtext.module.XTextSystemDescriptorServiceModule;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.PathMatcher;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.ServiceLoader;
-import java.util.stream.Collectors;
 
 public class CreateProtocolbufferMessagesCommandTest {
-
+   
    private IJellyFishCommand cmd = injector.getInstance(CreateProtocolbufferMessagesCommandGuiceWrapper.class);
+
    
    private IJellyFishCommandOptions options = Mockito.mock(IJellyFishCommandOptions.class);
    private IParameter<String> parameter = Mockito.mock(IParameter.class);
@@ -44,22 +35,20 @@ public class CreateProtocolbufferMessagesCommandTest {
 
    @Before
    public void setup() throws IOException{
-      Path sdDir = Paths.get("src", "test", "sd");
-      PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**.sd");
-      Collection<Path> sdFiles = Files.walk(sdDir).filter(matcher::matches).collect(Collectors.toSet());
-      ISystemDescriptorService sdService = injector.getInstance(ISystemDescriptorService.class);
-      IParsingResult result = sdService.parseFiles(sdFiles);
-      Assert.assertTrue(result.getIssues().toString(), result.isSuccessful());
-      ISystemDescriptor sd = result.getSystemDescriptor();
-      Mockito.when(options.getSystemDescriptor()).thenReturn(sd);
+//      Path sdDir = Paths.get("src", "test", "sd");
+//      PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**.sd");
+//      Collection<Path> sdFiles = Files.walk(sdDir).filter(matcher::matches).collect(Collectors.toSet());
+//      ISystemDescriptorService sdService = injector.getInstance(ISystemDescriptorService.class);
+//      IParsingResult result = sdService.parseFiles(sdFiles);
+//      Assert.assertTrue(result.getIssues().toString(), result.isSuccessful());
+//      ISystemDescriptor sd = result.getSystemDescriptor();
+//      Mockito.when(options.getSystemDescriptor()).thenReturn(sd);
    }
 
    @Test
    public void testCommand() {
-      DefaultParameterCollection collection = new DefaultParameterCollection();
-      collection.addParameter(parameter);
       
-      Mockito.when(options.getParameters()).thenReturn(collection);
+      //TODO Should we even inject the IJellyFishCommandProvider class or just mock it?
       
       cmd.run(options);
    }
@@ -70,10 +59,18 @@ public class CreateProtocolbufferMessagesCommandTest {
          bind(ILogService.class).to(PrintStreamLogService.class);
       }
    };
-
+   
+   private static final Module JELLYFISH_CMD_PROVIDER_MODULE = new AbstractModule() {
+      @Override
+      protected void configure() {
+        // bind(IJellyFishCommandProvider.class).to(JellyFishCommandProvider.class);
+      }
+   };
+   
    private static Collection<Module> getModules() {
       Collection<Module> modules = new ArrayList<>();
       modules.add(LOG_SERVICE_MODULE);
+      modules.add(JELLYFISH_CMD_PROVIDER_MODULE);
       for (Module dynamicModule : ServiceLoader.load(Module.class)) {
          if (!(dynamicModule instanceof LogServiceModule)) {
             modules.add(dynamicModule);
@@ -84,7 +81,7 @@ public class CreateProtocolbufferMessagesCommandTest {
       modules.add(XTextSystemDescriptorServiceModule.forStandaloneUsage());
       return modules;
    }
-
-   private static final Injector injector = Guice.createInjector(getModules());
    
+   private static final Injector injector = Guice.createInjector(getModules());
+  
 }
