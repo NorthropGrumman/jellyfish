36e7feebb15a7ffe418cacd58dd1670fd3453ac4
ThomasJHooper <thomas.hooper@ngc.com>
2017-07-21 07:35:24 -0500

    Add javadoc.

--- com.ngc.seaside.jellyfish/src/main/java/com/ngc/seaside/jellyfish/JellyFish.java
+++ com.ngc.seaside.jellyfish/src/main/java/com/ngc/seaside/jellyfish/JellyFish.java
@@ -1,10 +1,12 @@
 package com.ngc.seaside.jellyfish;
 
+import com.google.common.base.Preconditions;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Module;
 
+import com.ngc.blocs.service.log.api.ILogService;
 import com.ngc.seaside.command.api.IUsage;
 import com.ngc.seaside.jellyfish.api.IJellyFishCommand;
 import com.ngc.seaside.jellyfish.api.IJellyFishCommandOptions;
@@ -18,11 +20,22 @@ import java.util.ServiceLoader;
 
 public class JellyFish {
 
-   private JellyFishCommandProviderModule jellyFishCommandProvider;
-   private ProxyJellyFishCommandProvider proxyJellyFishCommandProvider = new ProxyJellyFishCommandProvider();
+   /**
+    * An implementation of {@code IJellyFishCommandProvider} that delegates to the field {@code
+    * jellyFishCommandProvider}.  This proxy will only work if {@code jellyFishCommandProvider} is not {@code null}.
+    * The field will be {@code null} until the real implementation is resolved from the injector.
+    */
+   private final ProxyJellyFishCommandProvider proxyJellyFishCommandProvider = new ProxyJellyFishCommandProvider();
 
    /**
-    * Main to run the JellyFish application..
+    * The real implementation of the {@code IJellyFishCommandProvider}.
+    */
+   private IJellyFishCommandProvider jellyFishCommandProvider;
+
+   private ILogService logService;
+
+   /**
+    * Main to run the JellyFish application.
     *
     * @param args the program arguments. The first argument should always be the name of the command in which to run
     *             followed by a list of parameters for that command.
@@ -31,12 +44,12 @@ public class JellyFish {
       try {
          run(args);
       } catch (IllegalArgumentException e) {
-         System.out.println("\nERROR: " + e.getMessage() + "\nTry running \"jellyfish help\" for usage information.");
+         System.err.format("%nERROR: %s%nTry running \"jellyfish help\" for usage information.%n", e.getMessage());
       }
    }
 
    /**
-    * Run the JellyFish application
+    * Run the JellyFish application.
     *
     * @param args the program arguments. The first argument should always be the name of the command in which to run
     *             followed by a list of parameters for that command.
@@ -47,6 +60,7 @@ public class JellyFish {
 
    private void doRun(String[] args) {
       Injector injector = getInjector();
+      // Note the module itself is an implementation of IJellyFishCommandProvider.
       jellyFishCommandProvider = injector.getInstance(JellyFishCommandProviderModule.class);
       jellyFishCommandProvider.run(args);
    }
@@ -68,56 +82,79 @@ public class JellyFish {
    private Collection<Module> getModules() {
       Collection<Module> modules = new ArrayList<>();
       modules.add(new JellyFishServiceModule());
+      // Add the proxy command provider module so that an IJellyFishCommandProvider can be resolved.
       modules.add(proxyJellyFishCommandProvider);
       for (Module dynamicModule : ServiceLoader.load(Module.class)) {
-         // TODO log this
-         System.out.println(String.format("%s", dynamicModule.getClass()));
-         modules.add(dynamicModule);
+         // Ignore the XTextSystemDescriptorServiceModule, we'll create the module below via forStandaloneUsage().  This
+         // is because XTextSystemDescriptorServiceModule is registered as an Module and the service loader picks it up.
+         // However, we need to build the module via forStandaloneUsage() to make sure the XText framework is
+         // initialized correctly.
+         if (dynamicModule.getClass() != XTextSystemDescriptorServiceModule.class) {
+            logService.debug(getClass(), String.format("Loaded module %s.", dynamicModule.getClass()));
+            modules.add(dynamicModule);
+         }
       }
-
-      // The following code was added because multiple instances of the {@link XTextSystemDescriptorServiceModule}
-      // were being created. This code can be removed once the system descriptor services have been updated
-      // to handle this situation.
-      modules.removeIf(m -> m instanceof XTextSystemDescriptorServiceModule);
+      // Register the standalone version of the XText service.
       modules.add(XTextSystemDescriptorServiceModule.forStandaloneUsage());
       return modules;
    }
 
+   /**
+    * A proxy of {@code IJellyFishCommandProvider} that will register itself with Guice.  This allows commands that are
+    * injected into the real implementation to be able to resolve a reference to the this proxy implementation of the
+    * command provider.  This provider can only be used after the injector has resolved the real implementation.  As
+    * long as a command does not use the service until its run method is invoked, this won't be a problem.  Note this
+    * is only an artifact of Guice; OSGi doesn't need this pattern.
+    */
    private class ProxyJellyFishCommandProvider extends AbstractModule implements IJellyFishCommandProvider {
 
       @Override
       public void run(String command, IJellyFishCommandOptions commandOptions) {
+         ensureProviderSet();
          jellyFishCommandProvider.run(command, commandOptions);
       }
 
       @Override
       public IUsage getUsage() {
+         ensureProviderSet();
          return jellyFishCommandProvider.getUsage();
       }
 
       @Override
       public IJellyFishCommand getCommand(String commandName) {
+         ensureProviderSet();
          return jellyFishCommandProvider.getCommand(commandName);
       }
 
       @Override
       public void addCommand(IJellyFishCommand command) {
+         ensureProviderSet();
          jellyFishCommandProvider.addCommand(command);
       }
 
       @Override
       public void removeCommand(IJellyFishCommand command) {
+         ensureProviderSet();
          jellyFishCommandProvider.removeCommand(command);
       }
 
       @Override
       public void run(String[] arguments) {
+         ensureProviderSet();
          jellyFishCommandProvider.run(arguments);
       }
 
       @Override
       protected void configure() {
+         // Bind IJellyFishCommandProvider to this proxy.
          bind(IJellyFishCommandProvider.class).toInstance(this);
+      }
+
+      private void ensureProviderSet() {
+         Preconditions.checkState(
+               jellyFishCommandProvider != null,
+               "proxy is not fully resolved, please don't use this proxy until the run() method of the command is"
+               + " called!");
       }
    }
 }
