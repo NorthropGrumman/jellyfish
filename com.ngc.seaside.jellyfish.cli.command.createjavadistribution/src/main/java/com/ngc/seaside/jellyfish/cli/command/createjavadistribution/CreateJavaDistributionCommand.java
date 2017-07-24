c006584185f640d3ea1ad9928ed2b84dcfff2d05
Valentine Nwachukwu <valentine.nwachukwu@ngc.com>
2017-07-24 10:17:00 -0500

    deferring from auto

--- com.ngc.seaside.jellyfish.cli.command.createjavadistribution/src/main/java/com/ngc/seaside/jellyfish/cli/command/createjavadistribution/CreateJavaDistributionCommand.java
+++ com.ngc.seaside.jellyfish.cli.command.createjavadistribution/src/main/java/com/ngc/seaside/jellyfish/cli/command/createjavadistribution/CreateJavaDistributionCommand.java
@@ -13,6 +13,7 @@ import com.ngc.seaside.command.api.IParameterCollection;
 import com.ngc.seaside.command.api.IUsage;
 import com.ngc.seaside.jellyfish.api.IJellyFishCommand;
 import com.ngc.seaside.jellyfish.api.IJellyFishCommandOptions;
+import com.ngc.seaside.jellyfish.api.JellyFishCommandConfiguration;
 import com.ngc.seaside.systemdescriptor.model.api.ISystemDescriptor;
 import com.ngc.seaside.systemdescriptor.model.api.model.IModel;
 
@@ -29,6 +30,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.regex.Pattern;
 
+@JellyFishCommandConfiguration(autoTemplateProcessing = false)
 @Component(service = IJellyFishCommand.class)
 public class CreateJavaDistributionCommand implements IJellyFishCommand {
 
