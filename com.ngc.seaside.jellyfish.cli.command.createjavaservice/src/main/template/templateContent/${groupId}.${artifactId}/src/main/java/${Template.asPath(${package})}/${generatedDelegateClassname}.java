f2b9486d20653a53ebb4bfae6f1697a436474683
Valentine Nwachukwu <valentine.nwachukwu@ngc.com>
2017-07-26 16:11:52 -0500

    fixed formating

--- com.ngc.seaside.jellyfish.cli.command.createjavaservice/src/main/template/templateContent/${groupId}.${artifactId}/src/main/java/${Template.asPath(${package})}/${generatedDelegateClassname}.java
+++ com.ngc.seaside.jellyfish.cli.command.createjavaservice/src/main/template/templateContent/${groupId}.${artifactId}/src/main/java/${Template.asPath(${package})}/${generatedDelegateClassname}.java
@@ -1,70 +1,68 @@
 package ${package};
 
-      import com.ngc.blocs.service.api.IServiceModule;
-      import com.ngc.blocs.service.event.api.IEventService;
-      import com.ngc.blocs.service.log.api.ILogService;
-      import com.ngc.seaside.service.fault.api.IFaultManagementService;
-      import com.ngc.seaside.service.fault.api.ServiceFaultException;
-      import com.ngc.seaside.service.fault.api.ServiceInputFaultException;
-      import com.ngc.seaside.threateval.engagementplanning.events.TrackEngagementStatus;
-      import ${groupId}.${artifactId}.api.I${modelname};
+import com.ngc.blocs.service.api.IServiceModule;
+import com.ngc.blocs.service.event.api.IEventService;
+import com.ngc.blocs.service.log.api.ILogService;
+import com.ngc.seaside.service.fault.api.IFaultManagementService;
+import com.ngc.seaside.service.fault.api.ServiceFaultException;
+import com.ngc.seaside.service.fault.api.ServiceInputFaultException;
+import com.ngc.seaside.threateval.engagementplanning.events.TrackEngagementStatus;
+import ${groupId}.${artifactId}.api.I${modelname};
 
-      import ${groupId}.${artifactId}.base.impl.Abstract${modelname};
-      import com.ngc.seaside.threateval.events.TrackPriority;
+import ${groupId}.${artifactId}.base.impl.Abstract${modelname};
+import com.ngc.seaside.threateval.events.TrackPriority;
 
-
-
-      import org.osgi.service.component.annotations.Activate;
-      import org.osgi.service.component.annotations.Component;
-      import org.osgi.service.component.annotations.Deactivate;
-      import org.osgi.service.component.annotations.Reference;
-      import org.osgi.service.component.annotations.ReferenceCardinality;
-      import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
 
 @Component(service = {I${modelname}.class, IServiceModule.class}, immediate = true)
 public class ${modelname} extends Abstract${modelname} {
 
-@Activate
-public void activate() {
+   @Activate
+   public void activate() {
       super.activate();
-      }
+   }
 
-@Deactivate
-public void deactivate() {
+   @Deactivate
+   public void deactivate() {
       super.deactivate();
-      }
+   }
 
-@Override
-@Reference(cardinality = ReferenceCardinality.MANDATORY, policy = ReferencePolicy.STATIC)
-public void setLogService(ILogService ref) {
+   @Override
+   @Reference(cardinality = ReferenceCardinality.MANDATORY, policy = ReferencePolicy.STATIC)
+   public void setLogService(ILogService ref) {
       super.setLogService(ref);
-      }
+   }
 
-@Override
-public void removeLogService(ILogService ref) {
+   @Override
+   public void removeLogService(ILogService ref) {
       super.removeLogService(ref);
-      }
+   }
 
-@Override
-@Reference(cardinality = ReferenceCardinality.MANDATORY, policy = ReferencePolicy.STATIC)
-public void setEventService(IEventService ref) {
+   @Override
+   @Reference(cardinality = ReferenceCardinality.MANDATORY, policy = ReferencePolicy.STATIC)
+   public void setEventService(IEventService ref) {
       super.setEventService(ref);
-      }
+   }
 
-@Override
-public void removeEventService(IEventService ref) {
+   @Override
+   public void removeEventService(IEventService ref) {
       super.removeEventService(ref);
-      }
+   }
 
-@Override
-@Reference(cardinality = ReferenceCardinality.MANDATORY, policy = ReferencePolicy.STATIC)
-public void setFaultManagementService(IFaultManagementService ref) {
+   @Override
+   @Reference(cardinality = ReferenceCardinality.MANDATORY, policy = ReferencePolicy.STATIC)
+   public void setFaultManagementService(IFaultManagementService ref) {
       super.setFaultManagementService(ref);
-      }
+   }
 
-@Override
-@Reference(cardinality = ReferenceCardinality.MANDATORY, policy = ReferencePolicy.STATIC)
-public void removeFaultManagementService(IFaultManagementService ref) {
+   @Override
+   @Reference(cardinality = ReferenceCardinality.MANDATORY, policy = ReferencePolicy.STATIC)
+   public void removeFaultManagementService(IFaultManagementService ref) {
       super.removeFaultManagementService(ref);
-      }
-      }
+   }
+}
