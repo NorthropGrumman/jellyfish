e1aaa050f7b77a159c4618ed4f9164bc3033b727
ThomasJHooper <thomas.hooper@ngc.com>
2017-07-28 08:08:13 -0500

    Create the create java service project command.

---
+++ com.ngc.seaside.jellyfish.cli.command.createjavaserviceproject/src/test/java/com/ngc/seaside/jellyfish/cli/command/createjavaserviceproject/CreateJavaServiceProjectCommandTest.java
@@ -0,0 +1,25 @@
+package com.ngc.seaside.jellyfish.cli.command.createjavaserviceproject;
+
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CreateJavaServiceProjectCommandTest {
+
+   private CreateJavaServiceProjectCommand command;
+
+
+   @Before
+   public void setup() {
+   }
+
+   @Test
+   public void testCommand() {
+      // TODO Auto-generated method stub
+   }
+   
+}
