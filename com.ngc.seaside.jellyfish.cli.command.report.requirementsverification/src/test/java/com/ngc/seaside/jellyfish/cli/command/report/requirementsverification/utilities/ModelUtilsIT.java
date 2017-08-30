fd636517fa3d11fb8fa1d5e0c2f9ac2b8e559e40
JasonDWest <jason.d.west@ngc.com>
2017-08-30 17:50:15 -0500

    Fixed problem of additional test files for CEAC-760 broke other tests.

--- com.ngc.seaside.jellyfish.cli.command.report.requirementsverification/src/test/java/com/ngc/seaside/jellyfish/cli/command/report/requirementsverification/utilities/ModelUtilsIT.java
+++ com.ngc.seaside.jellyfish.cli.command.report.requirementsverification/src/test/java/com/ngc/seaside/jellyfish/cli/command/report/requirementsverification/utilities/ModelUtilsIT.java
@@ -1,23 +1,8 @@
 package com.ngc.seaside.jellyfish.cli.command.report.requirementsverification.utilities;
 
-import static org.junit.Assert.*;
-
-import com.google.inject.AbstractModule;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-import com.google.inject.Module;
-import com.ngc.blocs.service.log.api.ILogService;
-import com.ngc.blocs.test.impl.common.log.PrintStreamLogService;
-import com.ngc.seaside.command.api.DefaultParameterCollection;
 import com.ngc.seaside.jellyfish.api.IJellyFishCommandOptions;
 import com.ngc.seaside.systemdescriptor.model.api.IPackage;
-import com.ngc.seaside.systemdescriptor.model.api.ISystemDescriptor;
 import com.ngc.seaside.systemdescriptor.model.api.model.IModel;
-import com.ngc.seaside.systemdescriptor.service.api.IParsingResult;
-import com.ngc.seaside.systemdescriptor.service.api.ISystemDescriptorService;
-import com.ngc.seaside.systemdescriptor.service.impl.xtext.module.XTextSystemDescriptorServiceModule;
-
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,53 +11,18 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.PathMatcher;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.stream.Collectors;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ModelUtilsIT {
-   private static final PrintStreamLogService logger = new PrintStreamLogService();
-   private static final Injector injector = Guice.createInjector(getModules());
-
-   private DefaultParameterCollection parameters;
-
    @Mock
    private IJellyFishCommandOptions jellyFishCommandOptions;
 
-   private static Collection<Module> getModules() {
-      Collection<Module> modules = new ArrayList<>();
-      modules.removeIf(m -> m instanceof XTextSystemDescriptorServiceModule);
-      modules.add(XTextSystemDescriptorServiceModule.forStandaloneUsage());
-      modules.add(new AbstractModule() {
-         @Override
-         protected void configure() {
-            bind(ILogService.class).toInstance(logger);
-         }
-      });
-      return modules;
-   }
-
    @Before
    public void setup() throws IOException {
-      parameters = new DefaultParameterCollection();
-      Mockito.when(jellyFishCommandOptions.getParameters()).thenReturn(parameters);
       Mockito.when(jellyFishCommandOptions.getSystemDescriptorProjectPath()).thenReturn(Paths.get("src/test/resources"));
-
-      // Setup mock system descriptor
-      Path sdDir = Paths.get("src", "test", "resources");
-      PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**.sd");
-      Collection<Path> sdFiles = Files.walk(sdDir).filter(matcher::matches).collect(Collectors.toSet());
-      ISystemDescriptorService sdService = injector.getInstance(ISystemDescriptorService.class);
-      IParsingResult result = sdService.parseFiles(sdFiles);
-      Assert.assertTrue(result.getIssues().toString(), result.isSuccessful());
-      ISystemDescriptor sd = result.getSystemDescriptor();
-      Mockito.when(jellyFishCommandOptions.getSystemDescriptor()).thenReturn(sd);
    }
 
    @Test
@@ -80,9 +30,10 @@ public class ModelUtilsIT {
       IPackage mockPackage = Mockito.mock(IPackage.class);         
       IModel mockModel = Mockito.mock(IModel.class);         
 
-      Mockito.when(mockPackage.getName()).thenReturn("com.ngc.seaside.testeval4");
+      Mockito.when(mockPackage.getName()).thenReturn("com.ngc.seaside.testeval3");
       Mockito.when(mockModel.getParent()).thenReturn(mockPackage);
       
+      // Need at least one model
       Collection<IModel> models = new ArrayList<>();
       models.add(mockModel);
       
