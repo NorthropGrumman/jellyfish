ba1234d005c9e52a85a0fc7552b4693234ec2467
Tyler King <Tyler.King@ngc.com>
2017-09-14 23:14:10 -0500

    Implement IJavaServiceGenerationService

--- com.ngc.seaside.jellyfish.service.codegen.impl.codegenservices/src/main/java/com/ngc/seaside/jellyfish/service/codegen/javaservice/impl/JavaServiceGenerationServiceGuiceWrapper.java
+++ com.ngc.seaside.jellyfish.service.codegen.impl.codegenservices/src/main/java/com/ngc/seaside/jellyfish/service/codegen/javaservice/impl/JavaServiceGenerationServiceGuiceWrapper.java
@@ -1,11 +1,14 @@
 package com.ngc.seaside.jellyfish.service.codegen.javaservice.impl;
 
 import com.google.inject.Inject;
-
 import com.ngc.blocs.service.log.api.ILogService;
 import com.ngc.seaside.jellyfish.api.IJellyFishCommandOptions;
 import com.ngc.seaside.jellyfish.service.codegen.api.IJavaServiceGenerationService;
 import com.ngc.seaside.jellyfish.service.codegen.api.dto.ClassDto;
+import com.ngc.seaside.jellyfish.service.codegen.api.dto.EnumDto;
+import com.ngc.seaside.jellyfish.service.codegen.api.dto.MethodDto;
+import com.ngc.seaside.jellyfish.service.codegen.api.dto.PubSubMethodDto;
+import com.ngc.seaside.jellyfish.service.config.api.ITransportConfigurationService;
 import com.ngc.seaside.jellyfish.service.name.api.IPackageNamingService;
 import com.ngc.seaside.jellyfish.service.scenario.api.IScenarioService;
 import com.ngc.seaside.systemdescriptor.model.api.model.IModel;
@@ -17,21 +20,29 @@ public class JavaServiceGenerationServiceGuiceWrapper implements IJavaServiceGen
    @Inject
    public JavaServiceGenerationServiceGuiceWrapper(ILogService logService,
                                                    IScenarioService scenarioService,
-                                                   IPackageNamingService packageNamingService) {
+                                                   IPackageNamingService packageNamingService,
+                                                   ITransportConfigurationService transportConfigService) {
       javaServiceGenerationService = new JavaServiceGenerationService();
       javaServiceGenerationService.setLogService(logService);
       javaServiceGenerationService.setScenarioService(scenarioService);
       javaServiceGenerationService.setPackageNamingService(packageNamingService);
+      javaServiceGenerationService.setTransportConfigurationService(transportConfigService);
       javaServiceGenerationService.activate();
    }
 
    @Override
-   public ClassDto getServiceInterfaceDescription(IJellyFishCommandOptions options, IModel model) {
+   public ClassDto<MethodDto> getServiceInterfaceDescription(IJellyFishCommandOptions options, IModel model) {
       return javaServiceGenerationService.getServiceInterfaceDescription(options, model);
    }
 
    @Override
-   public ClassDto getBaseServiceDescription(IJellyFishCommandOptions options, IModel model) {
+   public ClassDto<PubSubMethodDto> getBaseServiceDescription(IJellyFishCommandOptions options,
+            IModel model) {
       return javaServiceGenerationService.getBaseServiceDescription(options, model);
+   }
+
+   @Override
+   public EnumDto<?> getTransportTopicsDescription(IJellyFishCommandOptions options, IModel model) {
+      return javaServiceGenerationService.getTransportTopicsDescription(options, model);
    }
 }
