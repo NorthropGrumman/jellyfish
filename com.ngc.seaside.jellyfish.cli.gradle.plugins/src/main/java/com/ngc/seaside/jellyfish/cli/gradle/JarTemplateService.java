ed9f6cb4c25d712de8659c6f91bd2e0a43255349
ThomasJHooper <thomas.hooper@ngc.com>
2017-09-27 09:41:40 -0500

    Can now generated from gradle.

--- com.ngc.seaside.jellyfish.cli.gradle.plugins/src/main/java/com/ngc/seaside/jellyfish/cli/gradle/JarTemplateService.java
+++ com.ngc.seaside.jellyfish.cli.gradle.plugins/src/main/java/com/ngc/seaside/jellyfish/cli/gradle/JarTemplateService.java
@@ -1,5 +1,6 @@
 package com.ngc.seaside.jellyfish.cli.gradle;
 
+import com.google.common.base.Preconditions;
 import com.google.inject.AbstractModule;
 import com.google.inject.Inject;
 import com.google.inject.Module;
@@ -10,19 +11,55 @@ import com.ngc.seaside.bootstrap.service.impl.templateservice.TemplateService;
 import com.ngc.seaside.bootstrap.service.promptuser.api.IPromptUserService;
 import com.ngc.seaside.bootstrap.service.property.api.IPropertyService;
 import com.ngc.seaside.bootstrap.service.template.api.ITemplateService;
+import com.ngc.seaside.bootstrap.service.template.api.TemplateServiceException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class JarTemplateService extends TemplateService {
+
+   private final static String CONFIG_PROPERTIES_FILE = "com.ngc.seaside.jellyfish.cli.gradle.config.properties";
+   private final static String VERSION_PROPERTY = "version";
+
+   private final String version;
 
    @Inject
    public JarTemplateService(ILogService logService,
                              IResourceService resourceService,
                              IPromptUserService promptUserService,
                              IPropertyService propertyService) {
+      version = loadVersion();
       super.setLogService(logService);
       super.setResourceService(resourceService);
       super.setPromptUserService(promptUserService);
       super.setPropertyService(propertyService);
       super.activate();
+   }
+
+   @Override
+   protected InputStream getTemplateInputStream(String templateName) throws IOException, TemplateServiceException {
+      InputStream is;
+      is = getClass().getClassLoader().getResourceAsStream(String.format("templates/%s-%s-template.zip",
+                                                                         templateName,
+                                                                         version));
+      if (is == null) {
+         throw new TemplateServiceException("no template named " + templateName + " found!");
+      }
+      return is;
+   }
+
+   private static String loadVersion() {
+      String version;
+      try (InputStream is = JarTemplateService.class.getClassLoader().getResourceAsStream(CONFIG_PROPERTIES_FILE)) {
+         Properties props = new Properties();
+         props.load(is);
+         version = props.getProperty(VERSION_PROPERTY);
+         Preconditions.checkState(version != null, "version not set in configuration properties!");
+      } catch (IOException e) {
+         throw new IllegalStateException("failed to load configuration properties from classpath!", e);
+      }
+      return version;
    }
 
    public final static Module MODULE = new AbstractModule() {
