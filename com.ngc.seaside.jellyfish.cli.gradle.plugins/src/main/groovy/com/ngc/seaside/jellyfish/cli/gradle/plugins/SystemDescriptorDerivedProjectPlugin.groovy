d199c88e033c93289709c8e7bb5b30d4faee67f8
ThomasJHooper <thomas.hooper@ngc.com>
2017-09-27 12:20:52 -0500

    Create JellyFish plugin for derived generated projects.

--- com.ngc.seaside.jellyfish.cli.gradle.plugins/src/main/groovy/com/ngc/seaside/jellyfish/cli/gradle/plugins/SystemDescriptorDerivedProjectPlugin.groovy
+++ com.ngc.seaside.jellyfish.cli.gradle.plugins/src/main/groovy/com/ngc/seaside/jellyfish/cli/gradle/plugins/SystemDescriptorDerivedProjectPlugin.groovy
@@ -1,4 +1,33 @@
 package com.ngc.seaside.jellyfish.cli.gradle.plugins
 
-class SystemDescriptorDerivedProjectPlugin {
+import com.ngc.seaside.jellyfish.cli.gradle.JellyFishProjectGenerator
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class SystemDescriptorDerivedProjectPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project p) {
+        p.configure(p) {
+            task('clean-gen') {
+                doLast {
+                    delete 'src'
+                    delete 'build.generated.gradle'
+                }
+            }
+
+            if (!file("build.generated.gradle").exists()) {
+                logger.info(":generate")
+                new JellyFishProjectGenerator(logger)
+                      .setCommand('create-java-events')
+                      .setInputDir(file("${systemDescriptorLocation}").absolutePath)
+                      .setArguments(['model'               : "${modelName}",
+                                     'outputDirectory'     : "${project.rootDir.absolutePath}",
+                                     'updateGradleSettings': 'false'])
+                      .generate()
+            }
+
+            apply from: 'build.generated.gradle'
+        }
+    }
 }
