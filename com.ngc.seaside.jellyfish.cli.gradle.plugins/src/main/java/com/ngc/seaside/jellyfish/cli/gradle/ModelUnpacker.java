a07a9d8297a363e1b50a328895183d960cea3710
ThomasJHooper <thomas.hooper@ngc.com>
2017-09-27 12:03:06 -0500

    Add model unpacker and project generator.

---
+++ com.ngc.seaside.jellyfish.cli.gradle.plugins/src/main/java/com/ngc/seaside/jellyfish/cli/gradle/ModelUnpacker.java
@@ -0,0 +1,92 @@
+package com.ngc.seaside.jellyfish.cli.gradle;
+
+import com.google.common.base.Preconditions;
+
+import org.apache.commons.io.IOUtils;
+import org.gradle.api.GradleException;
+import org.gradle.api.artifacts.Configuration;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+public class ModelUnpacker {
+
+   private Path destinationDirectory;
+
+   private Configuration configuration;
+
+   public Collection<Path> unpack() {
+      Preconditions.checkNotNull(destinationDirectory, "destinationDirectory may not be null!");
+      Preconditions.checkNotNull(configuration, "configuration may not be null!");
+
+      File dest = destinationDirectory.toFile();
+      Preconditions.checkArgument(!dest.exists() || !dest.isDirectory(),
+                                  "%s exists but is not a directory!",
+                                  destinationDirectory);
+
+      if (!dest.isDirectory()) {
+         dest.mkdirs();
+      }
+
+      Collection<Path> unpackedFiles = new ArrayList<>();
+      for (File f : configuration) {
+         // Strip the extension.
+         File destForZip = new File(dest, f.getName().substring(0, f.getName().lastIndexOf('.')));
+         unpackedFiles.add(destForZip.toPath());
+         unpackFile(f, destForZip);
+      }
+      return unpackedFiles;
+   }
+
+   public Path getDestinationDirectory() {
+      return destinationDirectory;
+   }
+
+   public ModelUnpacker setDestinationDirectory(Path destinationDirectory) {
+      this.destinationDirectory = destinationDirectory;
+      return this;
+   }
+
+   public ModelUnpacker setDestinationDirectory(File destinationDirectory) {
+      this.destinationDirectory = destinationDirectory.toPath();
+      return this;
+   }
+
+   public Configuration getConfiguration() {
+      return configuration;
+   }
+
+   public ModelUnpacker setConfiguration(Configuration configuration) {
+      this.configuration = configuration;
+      return this;
+   }
+
+   private void unpackFile(File file, File dest) {
+      // Treat the file like a zip.
+      try (ZipFile zip = new ZipFile(file)) {
+         Enumeration<? extends ZipEntry> entries = zip.entries();
+         while (entries.hasMoreElements()) {
+            ZipEntry entry = entries.nextElement();
+            File entryDestination = new File(dest, entry.getName());
+            if (entry.isDirectory()) {
+               entryDestination.mkdirs();
+            } else {
+               entryDestination.getParentFile().mkdirs();
+               try (OutputStream out = new FileOutputStream(entryDestination)) {
+                  IOUtils.copy(zip.getInputStream(entry), out);
+               }
+            }
+         }
+      } catch (IOException e) {
+         throw new GradleException("failed to unzip " + file + "!", e);
+      }
+   }
+}
