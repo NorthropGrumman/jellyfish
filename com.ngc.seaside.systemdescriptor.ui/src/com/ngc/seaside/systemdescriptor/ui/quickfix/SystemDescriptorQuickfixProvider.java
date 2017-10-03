f3acd852261cb1151c45b8e18cbcdde092b91b1e
Tyler King <Tyler.King@ngc.com>
2017-10-02 19:52:18 -0500

    Generate quick fix provider in java

---
+++ com.ngc.seaside.systemdescriptor.ui/src/com/ngc/seaside/systemdescriptor/ui/quickfix/SystemDescriptorQuickfixProvider.java
@@ -0,0 +1,32 @@
+package com.ngc.seaside.systemdescriptor.ui.quickfix;
+
+import com.google.inject.Inject;
+import com.ngc.seaside.systemdescriptor.ui.quickfix.imports.ImportQuickfixProvider;
+
+import org.eclipse.xtext.ui.editor.quickfix.IssueResolution;
+import org.eclipse.xtext.ui.editor.quickfix.IssueResolutionProvider;
+import org.eclipse.xtext.validation.Issue;
+
+import java.util.List;
+
+/**
+ * Custom quickfixes.
+ *
+ * See https://www.eclipse.org/Xtext/documentation/310_eclipse_support.html#quick-fixes
+ */
+class SystemDescriptorQuickfixProvider implements IssueResolutionProvider {
+
+   @Inject
+   private ImportQuickfixProvider provider;
+
+   @Override
+   public List<IssueResolution> getResolutions(Issue issue) {
+      return provider.getResolutions(issue);
+   }
+
+   @Override
+   public boolean hasResolutionFor(String issue) {
+      return provider.hasResolutionFor(issue);
+   }
+
+}
