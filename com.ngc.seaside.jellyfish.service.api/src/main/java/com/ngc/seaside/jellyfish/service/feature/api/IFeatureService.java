dbdb2ec544ff7d32610a85379f5fe45a97fcfb5d
ThomasJHooper <thomas.hooper@ngc.com>
2017-10-18 11:57:02 -0500

    Add new method to feature service.

--- com.ngc.seaside.jellyfish.service.api/src/main/java/com/ngc/seaside/jellyfish/service/feature/api/IFeatureService.java
+++ com.ngc.seaside.jellyfish.service.api/src/main/java/com/ngc/seaside/jellyfish/service/feature/api/IFeatureService.java
@@ -1,6 +1,7 @@
 package com.ngc.seaside.jellyfish.service.feature.api;
 
 import com.ngc.seaside.systemdescriptor.model.api.model.IModel;
+import com.ngc.seaside.systemdescriptor.model.api.model.scenario.IScenario;
 
 import java.nio.file.Path;
 import java.util.Collection;
@@ -21,11 +22,18 @@ public interface IFeatureService {
    TreeMap<String, IFeatureInformation> getFeatures(Path sdPath, IModel model);
 
    /**
+    * Gets a map of all the features for the given scenario.
+    * @param sdPath the path to the system descriptor
+    * @param scenario the scenario to get the features for
+    * @return a map of the feature information for the given scenario
+    */
+   TreeMap<String, IFeatureInformation> getFeatures(Path sdPath, IScenario scenario);
+
+   /**
     * Gets a map of all the features for the given system descriptor path and a collection of models
     * @param sdPath the path to the system descriptor
     * @param models the collection of models
     * @return map of the feature information
     */
    TreeMap<String, IFeatureInformation> getAllFeatures(Path sdPath, Collection<IModel> models);
-
 }
