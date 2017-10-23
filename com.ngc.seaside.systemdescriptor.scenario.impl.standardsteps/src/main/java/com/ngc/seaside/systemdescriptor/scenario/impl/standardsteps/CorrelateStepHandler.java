d8db4ecbe7869ab29dc5bebfb8b6cfb3e86c29e8
perkinsb <Blake.Perkins@ngc.com>
2017-10-23 16:07:19 -0500

    Changed approach and updated test

--- com.ngc.seaside.systemdescriptor.scenario.impl.standardsteps/src/main/java/com/ngc/seaside/systemdescriptor/scenario/impl/standardsteps/CorrelateStepHandler.java
+++ com.ngc.seaside.systemdescriptor.scenario.impl.standardsteps/src/main/java/com/ngc/seaside/systemdescriptor/scenario/impl/standardsteps/CorrelateStepHandler.java
@@ -1,8 +1,6 @@
 package com.ngc.seaside.systemdescriptor.scenario.impl.standardsteps;
 
 import com.google.common.base.Preconditions;
-import com.ngc.seaside.systemdescriptor.model.api.model.IDataReferenceField;
-import com.ngc.seaside.systemdescriptor.model.api.model.IModel;
 import com.ngc.seaside.systemdescriptor.model.api.model.scenario.IScenarioStep;
 import com.ngc.seaside.systemdescriptor.scenario.api.AbstractStepHandler;
 import com.ngc.seaside.systemdescriptor.scenario.api.ScenarioStepVerb;
@@ -10,8 +8,6 @@ import com.ngc.seaside.systemdescriptor.validation.api.IValidationContext;
 import com.ngc.seaside.systemdescriptor.validation.api.Severity;
 
 import java.util.List;
-import java.util.StringJoiner;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Implements the "correlate" verb which is used to indicate multiple pieces of data must be correlated together.
@@ -27,8 +23,19 @@ public class CorrelateStepHandler extends AbstractStepHandler {
    public final static ScenarioStepVerb PRESENT = ScenarioStepVerb.presentTense("correlating");
    public final static ScenarioStepVerb FUTURE = ScenarioStepVerb.futureTense("willCorrelate");
 
+   private String leftData;
+   private String rightData;
+
    public CorrelateStepHandler() {
       register(PRESENT, FUTURE);
+   }
+
+   public String getLeftData() {
+      return leftData;
+   }
+
+   public String getRightData() {
+      return rightData;
    }
 
    @Override
@@ -44,69 +51,48 @@ public class CorrelateStepHandler extends AbstractStepHandler {
             step)
                 .getKeyword();
       } else {
-         
-         IDataReferenceField inputs = getInputs(step);
-         IDataReferenceField outputs = getOutputs(step);
+
+         leftData = getCorrelationArg(step, 0);
+         validateToArgument(context, step, 1);
+         rightData = getCorrelationArg(step, 2);
+
          // The first parameter should be an operand.
-       //  validateOperand(context, step, parameters.get(0));
+         // validateOperand(context, step, parameters.get(0));
          // The second parameter should be a number (double).
-     //    validateDuration(context, step, parameters.get(1));
+         // validateDuration(context, step, parameters.get(1));
          // The third parameter should be a time unit.
-       //  validateTimeUnit(context, step, parameters.get(2));
+         // validateTimeUnit(context, step, parameters.get(2));
       }
 
    }
 
-   /**
-    * Gets the {@code IDataReferenceField} of the input of the model the scenario is associated with that this step.
-    * This can be used to determine which input field is published.
-    *
-    * <p/>
-    *
-    * Only invoke this method with validated scenario steps.
-    *
-    * @param step the step that contains a publish verb.
-    * @return the input field of the model that is published
-    */
-   public IDataReferenceField getInputs(IScenarioStep step) {
+   private String getCorrelationArg(IScenarioStep step, int argPosition) {
       Preconditions.checkNotNull(step, "step may not be null!");
       String keyword = step.getKeyword();
       Preconditions.checkArgument(
          keyword.equals(PRESENT.getVerb())
             || keyword.equals(FUTURE.getVerb()),
          "the step cannot be processed by this handler!");
-
-      IModel model = step.getParent().getParent();
-      String inputName = step.getParameters().get(1);
-      return model.getInputs()
-                  .getByName(inputName)
-                  .orElseThrow(() -> new IllegalStateException("model does not contain an input named " + inputName));
+      return step.getParameters().get(argPosition);
    }
 
-   /**
-    * Gets the {@code IDataReferenceField} of the output of the model the scenario is associated with that this step.
-    * This can be used to determine which output field is published.
-    *
-    * <p/>
-    *
-    * Only invoke this method with validated scenario steps.
-    *
-    * @param step the step that contains a publish verb
-    * @return the output field of the model that is published
-    */
-   public IDataReferenceField getOutputs(IScenarioStep step) {
+   private void validateToArgument(IValidationContext<IScenarioStep> context, IScenarioStep step, int argPosition) {
       Preconditions.checkNotNull(step, "step may not be null!");
-      String keyword = step.getKeyword();
-      Preconditions.checkArgument(
-         keyword.equals(PRESENT.getVerb())
-            || keyword.equals(FUTURE.getVerb()),
-         "the step cannot be processed by this handler!");
-
-      IModel model = step.getParent().getParent();
-      String outputName = step.getParameters().get(0);
-      return model.getOutputs()
-                  .getByName(outputName)
-                  .orElseThrow(() -> new IllegalStateException("model does not contain an output named " + outputName));
+      if (step.getParameters().get(argPosition) != "to") {
+         declareOrThrowError(context,
+            step,
+            String.format("Expected parameter to be 'to'"));
+      }
+   
    }
 
+   private static void declareOrThrowError(IValidationContext<IScenarioStep> context, 
+                                           IScenarioStep step,
+                                           String errMessage) {
+      if (context != null) {
+         context.declare(Severity.ERROR, errMessage, step).getKeyword();
+      } else {
+         throw new IllegalArgumentException(errMessage);
+      }
+   }
 }
--- com.ngc.seaside.systemdescriptor.scenario.impl.standardsteps/src/test/java/com/ngc/seaside/systemdescriptor/scenario/impl/standardsteps/CorrelateStepHandlerTest.java
+++ com.ngc.seaside.systemdescriptor.scenario.impl.standardsteps/src/test/java/com/ngc/seaside/systemdescriptor/scenario/impl/standardsteps/CorrelateStepHandlerTest.java
@@ -1,17 +1,25 @@
 package com.ngc.seaside.systemdescriptor.scenario.impl.standardsteps;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import com.ngc.seaside.systemdescriptor.model.impl.basic.data.Data;
-import com.ngc.seaside.systemdescriptor.model.impl.basic.model.DataReferenceField;
-import com.ngc.seaside.systemdescriptor.model.impl.basic.model.Model;
-import com.ngc.seaside.systemdescriptor.model.impl.basic.model.scenario.Scenario;
+import com.ngc.seaside.systemdescriptor.model.api.model.scenario.IScenarioStep;
 import com.ngc.seaside.systemdescriptor.model.impl.basic.model.scenario.ScenarioStep;
+import com.ngc.seaside.systemdescriptor.scenario.api.VerbTense;
+import com.ngc.seaside.systemdescriptor.validation.api.IValidationContext;
+import com.ngc.seaside.systemdescriptor.validation.api.Severity;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Arrays;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CorrelateStepHandlerTest {
@@ -20,49 +28,35 @@ public class CorrelateStepHandlerTest {
 
    private ScenarioStep step;
 
-   private Data data;
-
-   private DataReferenceField outputField;
-   
-   private DataReferenceField inputField;
+   @Mock
+   private IValidationContext<IScenarioStep> context;
 
    @Before
    public void setup() throws Throwable {
-      data = new Data("TestData");
-
-      outputField = new DataReferenceField("output1");
-      outputField.setType(data);
-      
-      inputField = new DataReferenceField("input1");
-      inputField.setType(data);
-
-      Model model = new Model("TestModel");
-      model.addOutput(outputField);
-      model.addInput(inputField);
-
-      Scenario scenario = new Scenario("test");
-      scenario.setParent(model);
+      handler = new CorrelateStepHandler();
 
       step = new ScenarioStep();
-      step.setKeyword(CorrelateStepHandler.PRESENT.getVerb());
-      step.getParameters().add(outputField.getName());
-      step.getParameters().add(inputField.getName());
-      step.setParent(scenario);
+      step.setKeyword(CorrelateStepHandler.FUTURE.getVerb());
 
-      handler = new CorrelateStepHandler();
+      when(context.getObject()).thenReturn(step);
    }
 
    @Test
-   public void testGetOutputs() throws Throwable {
-      assertEquals("did not return correct data!",
-         outputField,
-         handler.getOutputs(step));
+   public void testDoesRegisterVerbs() throws Throwable {
+
+      assertEquals("did not register correct present tense!",
+         CorrelateStepHandler.PRESENT,
+         handler.getVerbs().get(VerbTense.PRESENT_TENSE));
+      assertEquals("did not register correct future tense!",
+         CorrelateStepHandler.FUTURE,
+         handler.getVerbs().get(VerbTense.FUTURE_TENSE));
    }
-   
+
    @Test
-   public void testGetInputs() throws Throwable {
-      assertEquals("did not return correct data!",
-         inputField,
-         handler.getInputs(step));
+   public void testDoesValidateValidUsage() throws Throwable {
+      step.getParameters().addAll(Arrays.asList("a.foo", "to", "b.foo"));
+      handler.doValidateStep(context);
+      verify(context, never()).declare(eq(Severity.ERROR), anyString(), eq(step));
    }
+
 }
