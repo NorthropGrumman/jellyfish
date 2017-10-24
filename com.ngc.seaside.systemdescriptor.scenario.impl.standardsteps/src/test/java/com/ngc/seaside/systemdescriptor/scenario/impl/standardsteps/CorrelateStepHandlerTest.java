8dd581ccf67ee6675bcec9cba153b17950f914cd
perkinsb <Blake.Perkins@ngc.com>
2017-10-24 13:50:06 -0500

    Added modelutils so testing will be easier

--- com.ngc.seaside.systemdescriptor.scenario.impl.standardsteps/src/test/java/com/ngc/seaside/systemdescriptor/scenario/impl/standardsteps/CorrelateStepHandlerTest.java
+++ com.ngc.seaside.systemdescriptor.scenario.impl.standardsteps/src/test/java/com/ngc/seaside/systemdescriptor/scenario/impl/standardsteps/CorrelateStepHandlerTest.java
@@ -3,10 +3,20 @@ package com.ngc.seaside.systemdescriptor.scenario.impl.standardsteps;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.ngc.seaside.systemdescriptor.model.api.FieldCardinality;
+import com.ngc.seaside.systemdescriptor.model.api.INamedChildCollection;
+import com.ngc.seaside.systemdescriptor.model.api.data.DataTypes;
+import com.ngc.seaside.systemdescriptor.model.api.data.IData;
+import com.ngc.seaside.systemdescriptor.model.api.data.IDataField;
+import com.ngc.seaside.systemdescriptor.model.api.data.IEnumeration;
+import com.ngc.seaside.systemdescriptor.model.api.model.IDataReferenceField;
+import com.ngc.seaside.systemdescriptor.model.api.model.IModel;
+import com.ngc.seaside.systemdescriptor.model.api.model.scenario.IScenario;
 import com.ngc.seaside.systemdescriptor.model.api.model.scenario.IScenarioStep;
 import com.ngc.seaside.systemdescriptor.model.impl.basic.model.scenario.ScenarioStep;
 import com.ngc.seaside.systemdescriptor.scenario.api.VerbTense;
@@ -19,9 +29,14 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.AbstractSet;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(MockitoJUnitRunner.Silent.class)
 public class CorrelateStepHandlerTest {
 
    private CorrelateStepHandler handler;
@@ -34,11 +49,6 @@ public class CorrelateStepHandlerTest {
    @Before
    public void setup() throws Throwable {
       handler = new CorrelateStepHandler();
-
-      step = new ScenarioStep();
-      step.setKeyword(CorrelateStepHandler.FUTURE.getVerb());
-
-      when(context.getObject()).thenReturn(step);
    }
 
    @Test
@@ -54,9 +64,24 @@ public class CorrelateStepHandlerTest {
 
    @Test
    public void testDoesValidateValidUsage() throws Throwable {
+      step = new ScenarioStep();
+      step.setKeyword(CorrelateStepHandler.PRESENT.getVerb());
+      when(context.getObject()).thenReturn(step);
+
       step.getParameters().addAll(Arrays.asList("a.foo", "to", "b.foo"));
       handler.doValidateStep(context);
       verify(context, never()).declare(eq(Severity.ERROR), anyString(), eq(step));
    }
+
+   @Test
+   public void testValidPresentVerb() throws Throwable {
+
+      step = new ScenarioStep();
+      step.setKeyword(CorrelateStepHandler.PRESENT.getVerb());
+
+      when(context.getObject()).thenReturn(step);
+
+   }
+
 
 }
