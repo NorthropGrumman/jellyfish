216932170d3a32cfe2af5222e885980e905f13ff
Tyler King <Tyler.King@ngc.com>
2017-10-24 11:51:52 -0500

    Fix import quick fix provider for fully-qualified name references

--- com.ngc.seaside.systemdescriptor.ui/src/com/ngc/seaside/systemdescriptor/ui/quickfix/imports/IImportsResolver.java
+++ com.ngc.seaside.systemdescriptor.ui/src/com/ngc/seaside/systemdescriptor/ui/quickfix/imports/IImportsResolver.java
@@ -1,23 +1,16 @@
 package com.ngc.seaside.systemdescriptor.ui.quickfix.imports;
 
-import java.util.AbstractMap.SimpleImmutableEntry;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Optional;
-import java.util.OptionalInt;
-import java.util.Set;
-import java.util.concurrent.CancellationException;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
+import com.google.inject.ImplementedBy;
+import com.google.inject.Inject;
+import com.ngc.seaside.systemdescriptor.systemDescriptor.Import;
+import com.ngc.seaside.systemdescriptor.systemDescriptor.Package;
+import com.ngc.seaside.systemdescriptor.systemDescriptor.impl.ImportImpl;
 
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.util.EContentsEList.FeatureIterator;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.xtext.naming.IQualifiedNameConverter;
 import org.eclipse.xtext.naming.QualifiedName;
@@ -29,21 +22,17 @@ import org.eclipse.xtext.resource.IResourceDescriptionsProvider;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.util.ITextRegion;
 
-import com.google.common.collect.Iterables;
-import com.google.inject.ImplementedBy;
-import com.google.inject.Inject;
-import com.ngc.seaside.systemdescriptor.systemDescriptor.Data;
-import com.ngc.seaside.systemdescriptor.systemDescriptor.DataModel;
-import com.ngc.seaside.systemdescriptor.systemDescriptor.Import;
-import com.ngc.seaside.systemdescriptor.systemDescriptor.InputDeclaration;
-import com.ngc.seaside.systemdescriptor.systemDescriptor.Model;
-import com.ngc.seaside.systemdescriptor.systemDescriptor.OutputDeclaration;
-import com.ngc.seaside.systemdescriptor.systemDescriptor.Package;
-import com.ngc.seaside.systemdescriptor.systemDescriptor.PartDeclaration;
-import com.ngc.seaside.systemdescriptor.systemDescriptor.ReferencedDataModelFieldDeclaration;
-import com.ngc.seaside.systemdescriptor.systemDescriptor.RequireDeclaration;
-import com.ngc.seaside.systemdescriptor.systemDescriptor.SystemDescriptorPackage;
-import com.ngc.seaside.systemdescriptor.systemDescriptor.impl.ImportImpl;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.Set;
+import java.util.concurrent.CancellationException;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * Interface for determining the list of imports that a package needs.
@@ -102,35 +91,51 @@ class DefaultImportsResolver implements IImportsResolver {
    public List<Import> resolveImports(Package pkg, XtextResource resource) {
       final IResourceDescriptions descriptions = resourceDescriptionsProvider.getResourceDescriptions(
          pkg.eResource().getResourceSet());
-      final Set<QualifiedName> names = new HashSet<>();
+      final Set<QualifiedName> namesToImport = new HashSet<>();
       TreeIterator<EObject> contents = EcoreUtil.getAllContents(pkg, true);
-      Map<String, QualifiedName> checkedUnresolvedReferences = new HashMap<>();
+      Map<String, QualifiedName> resolvedReferences = new HashMap<>();
+
+      // Search all of the contents of the package for external references
       while (contents.hasNext()) {
          EObject element = contents.next();
 
-         for (EObject reference : element.eCrossReferences()) {
-            List<IEObjectDescription> referenceDescriptions = new ArrayList<>();
-            Iterables.addAll(referenceDescriptions, descriptions.getExportedObjectsByObject(reference));
-            if (referenceDescriptions.isEmpty()) {
-               Entry<EReference, Predicate<? super EObject>> ref = getReference(element);
-               ITextRegion refRegion = locationInFileProvider.getSignificantTextRegion(element, ref.getKey(), 0);
+         // Search the cross-references of the given element for external references
+         for (FeatureIterator<EObject> iter = (FeatureIterator<EObject>) element.eCrossReferences()
+                                                                                .iterator(); iter.hasNext();) {
+            EObject referencedElement = iter.next();
+            EStructuralFeature feature = iter.feature();
+            ITextRegion refRegion = locationInFileProvider.getSignificantTextRegion(element, feature, 0);
+            String referencedName = getReferencedType(resource, refRegion);
+            if (referencedName.isEmpty() || referencedName.indexOf('.') >= 0) {
+               // Don't import references that use fully-qualified names
+               continue;
+            }
+            
+            boolean isUnresolved = referencedElement.eIsProxy();
+            if (isUnresolved) {
+               // Resource has not been resolved
+               // Search for potential references and select one
+               Predicate<? super EObject> predicate = obj -> referencedElement.eClass().isInstance(obj);
                Optional<QualifiedName> choice = findUnknownReference(resource,
                   resource.getResourceSet(),
-                  checkedUnresolvedReferences,
+                  resolvedReferences,
+                  referencedName,
                   refRegion,
-                  ref.getValue());
+                  predicate);
                if (choice.isPresent()) {
-                  names.add(choice.get());
+                  namesToImport.add(choice.get());
                }
             } else {
-               for (IEObjectDescription description : descriptions.getExportedObjectsByObject(reference)) {
-                  names.add(description.getQualifiedName());
+               // Resource has been resolve
+               // Add resources to imports set
+               for (IEObjectDescription description : descriptions.getExportedObjectsByObject(referencedElement)) {
+                  namesToImport.add(description.getQualifiedName());
                }
             }
          }
       }
-
-      return names.stream().map(name -> new ImportImpl() {
+      
+      return namesToImport.stream().map(name -> new ImportImpl() {
          @Override
          public String getImportedNamespace() {
             return qualifiedNameConverter.toString(name);
@@ -138,32 +143,20 @@ class DefaultImportsResolver implements IImportsResolver {
       }).collect(Collectors.toList());
    }
 
-   private Entry<EReference, Predicate<? super EObject>> getReference(EObject element) {
-      final Predicate<EObject> DATA_INSTANCE = object -> object instanceof Data;
-      final Predicate<EObject> DATA_MODEL_INSTANCE = object -> object instanceof DataModel;
-      final Predicate<EObject> MODEL_INSTANCE = object -> object instanceof Model;
-      if (element instanceof ReferencedDataModelFieldDeclaration) {
-         return new SimpleImmutableEntry<>(
-            SystemDescriptorPackage.Literals.REFERENCED_DATA_MODEL_FIELD_DECLARATION__DATA_MODEL, DATA_MODEL_INSTANCE);
+   /**
+    * Returns the actual text of a reference.
+    * 
+    * @param resource resource containing text
+    * @param region region of reference
+    * @return actual text of reference
+    */
+   private String getReferencedType(XtextResource resource, ITextRegion region) {
+      IParseResult parseResult = resource.getParseResult();
+      if (parseResult != null && region != null) {
+         return parseResult.getRootNode().getText().substring(region.getOffset(),
+            region.getOffset() + region.getLength());
       }
-      if (element instanceof Data) {
-         return new SimpleImmutableEntry<>(SystemDescriptorPackage.Literals.DATA__SUPERCLASS, DATA_INSTANCE);
-      }
-      if (element instanceof InputDeclaration) {
-         return new SimpleImmutableEntry<>(SystemDescriptorPackage.Literals.INPUT_DECLARATION__TYPE, DATA_INSTANCE);
-      }
-      if (element instanceof OutputDeclaration) {
-         return new SimpleImmutableEntry<>(SystemDescriptorPackage.Literals.OUTPUT_DECLARATION__TYPE, DATA_INSTANCE);
-      }
-      if (element instanceof PartDeclaration) {
-         return new SimpleImmutableEntry<>(SystemDescriptorPackage.Literals.PART_DECLARATION__TYPE, MODEL_INSTANCE);
-      }
-      if (element instanceof RequireDeclaration) {
-         return new SimpleImmutableEntry<>(SystemDescriptorPackage.Literals.REQUIRE_DECLARATION__TYPE,
-            MODEL_INSTANCE);
-      }
-
-      throw new IllegalStateException("Unknown reference for type " + element.eClass().getName());
+      return "";
    }
 
    /**
@@ -179,31 +172,21 @@ class DefaultImportsResolver implements IImportsResolver {
     */
    private Optional<QualifiedName> findUnknownReference(XtextResource resource, ResourceSet resourceSet,
             Map<String, QualifiedName> references,
+            String referenceText,
             ITextRegion region, Predicate<? super EObject> choiceFilter) {
-      if (region.getLength() > 0) {
-         IParseResult parseResult = resource.getParseResult();
-         if (parseResult != null) {
-            String completeText = parseResult.getRootNode().getText();
-            String refText = completeText.substring(region.getOffset(),
-               region.getOffset() + region.getLength());
-            if (references.containsKey(refText)) {
-               return Optional.of(references.get(refText));
-            }
-            Set<QualifiedName> possibleTypes = referenceResolver.findPossibleTypes(refText, resourceSet, choiceFilter);
-            if (possibleTypes.isEmpty()) {
-               return Optional.empty();
-            }
-            Optional<QualifiedName> name = selectReference(resource,
-               possibleTypes,
-               region);
-            if (!name.isPresent()) {
-               throw new CancellationException();
-            }
-            references.put(refText, name.get());
-            return name;
-         }
+      if (references.containsKey(referenceText)) {
+         return Optional.of(references.get(referenceText));
       }
-      return Optional.empty();
+      Set<QualifiedName> possibleTypes = referenceResolver.findPossibleTypes(referenceText, resourceSet, choiceFilter);
+      if (possibleTypes.isEmpty()) {
+         return Optional.empty();
+      }
+      Optional<QualifiedName> name = selectReference(resource, possibleTypes, region);
+      if (!name.isPresent()) {
+         throw new CancellationException();
+      }
+      references.put(referenceText, name.get());
+      return name;
    }
 
    /**
