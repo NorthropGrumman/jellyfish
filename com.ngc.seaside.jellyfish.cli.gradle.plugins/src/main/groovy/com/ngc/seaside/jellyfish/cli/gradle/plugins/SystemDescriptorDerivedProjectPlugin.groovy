24c05e9d3b4ce894e292d7429893983ef718580b
Tyler King <Tyler.King@ngc.com>
2017-11-20 09:05:33 -0600

    Refactor clean-gen task to automatically run when jellyfish cli dependencies have changed

--- com.ngc.seaside.jellyfish.cli.gradle.plugins/src/main/groovy/com/ngc/seaside/jellyfish/cli/gradle/plugins/SystemDescriptorDerivedProjectPlugin.groovy
+++ com.ngc.seaside.jellyfish.cli.gradle.plugins/src/main/groovy/com/ngc/seaside/jellyfish/cli/gradle/plugins/SystemDescriptorDerivedProjectPlugin.groovy
@@ -3,21 +3,47 @@ package com.ngc.seaside.jellyfish.cli.gradle.plugins
 import com.ngc.seaside.jellyfish.cli.gradle.JellyFishProjectGenerator
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.GradleException
 
 class SystemDescriptorDerivedProjectPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project p) {
         p.configure(p) {
-            task('clean-gen') {
-                doLast {
-                    delete 'src'
-                    delete 'build.generated.gradle'
+
+            // Find jellyfish-cli dependency from classpath
+            def cliDependency
+            def proj = p
+            while (true) {
+                cliDependency = proj.buildscript
+                                    .configurations
+                                    .classpath
+                                    .dependencies
+                                    .find { it.group == 'com.ngc.seaside' && it.name == 'jellyfish.cli.gradle.plugins'}
+                if (cliDependency != null) {
+                    break
+                }
+                proj = proj.parent
+                if (proj == null) {
+                    throw new GradleException('com.ngc.seaside.jellyfish.system-descriptor-derived plugin requires com.ngc.seaside.jellyfish.cli.gradle.plugins on the classpath')
                 }
             }
 
+            // Generate md5 for jellyfish-cli dependency and its transitive dependencies
+            def md5 = generateMD5(proj.buildscript.configurations.classpath.files(cliDependency))
+            def md5File = file("${p.projectDir}/.cli-dependencies.md5")
+
+            // Run clean-gen if anything in jellyfish-cli or its dependencies have changed
+            if (!md5File.exists() || md5File.text != md5 || gradle.startParameter.taskNames.contains('clean-gen')) {
+                logger.info(":${p.name}:clean-gen")
+                delete 'src'
+                delete 'build.generated.gradle'
+                md5File.text = md5
+            }
+
+            // test change
             if (!file("${project.projectDir}/build.generated.gradle").exists()) {
-                logger.info(":generate")
+                logger.info(":${p.name}:generate")
                 new JellyFishProjectGenerator(logger)
                       .setCommand("${command}")
                       .setInputDir(file("${systemDescriptorLocation}").absolutePath)
@@ -29,5 +55,16 @@ class SystemDescriptorDerivedProjectPlugin implements Plugin<Project> {
 
             apply from: "${project.projectDir}/build.generated.gradle"
         }
+    }
+
+    private def generateMD5(Iterable<File> files) {
+        def digest = java.security.MessageDigest.getInstance('MD5')
+        files.sort { it.name }.each {
+            digest.update(it.name.bytes)
+            it.eachByte( 4096 ) { buffer, length ->
+                digest.update(buffer, 0, length)
+            }
+        }
+        return digest.digest().encodeHex().toString()
     }
 }
