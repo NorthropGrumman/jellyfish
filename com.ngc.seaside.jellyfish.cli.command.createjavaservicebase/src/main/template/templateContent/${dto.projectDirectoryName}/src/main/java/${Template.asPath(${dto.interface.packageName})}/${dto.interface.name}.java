3e0842809e05112de47aa3df8b119054acd6888e
Tyler King <Tyler.King@ngc.com>
2017-11-27 15:19:10 -0600

    Add complex pub/sub scenarios to interface template

--- com.ngc.seaside.jellyfish.cli.command.createjavaservicebase/src/main/template/templateContent/${dto.projectDirectoryName}/src/main/java/${Template.asPath(${dto.interface.packageName})}/${dto.interface.name}.java
+++ com.ngc.seaside.jellyfish.cli.command.createjavaservicebase/src/main/template/templateContent/${dto.projectDirectoryName}/src/main/java/${Template.asPath(${dto.interface.packageName})}/${dto.interface.name}.java
@@ -11,18 +11,28 @@ public interface ${dto.interface.name}#if ($dto.interface.implementedInterface) 
    ${method.outputType} ${method.serviceName}(${method.inputType} input) throws ServiceFaultException;
    
 #end
-
 #foreach ($method in $dto.basicSinkMethods)
-   void ${method.outputType} ${method.serviceName}(${method.inputType} input) throws ServiceFaultException;
+   void ${method.serviceName}(${method.inputType} input) throws ServiceFaultException;
 
 #end
-
 #foreach ($method in $dto.correlationMethods)
    ${method.output.type} ${method.serviceName}(
 #foreach ($input in $method.inputs)
       ${input.type} ${input.inputArgumentString},
 #end
       ILocalCorrelationEvent<${method.correlationType}> correlationEvent) throws ServiceFaultException;
+
+#end
+#foreach ($scenario in $dto.complexScenarios)
+   void ${scenario.serviceName}(
+#foreach ($input in $scenario.inputs)
+#set ($lastParam = ${velocityCount == scenario.inputs.size() && scenario.outputs.isEmpty()})
+      BlockingQueue<${input.type}> input${velocityCount}Queue#if ($lastParam));#{else},#end
+#end
+#foreach ($output in $scenario.outputs)
+#set ($lastParam = ${velocityCount == scenario.outputs.size()})
+      Consumer<${output.type}> output${velocityCount}Consumer#if ($lastParam));#{else},#end
 #end
 
+#end
 }
