9d7c196e2da5a0b8a3397d7e3c9bf2201fe404fb
Tyler King <Tyler.King@ngc.com>
2017-12-13 17:19:16 -0600

    Add enum option to jellyfish file wizard

--- com.ngc.seaside.systemdescriptor.ui/src/com/ngc/seaside/systemdescriptor/ui/wizard/file/page/FilePage.java
+++ com.ngc.seaside.systemdescriptor.ui/src/com/ngc/seaside/systemdescriptor/ui/wizard/file/page/FilePage.java
@@ -33,6 +33,7 @@ public class FilePage extends WizardPage
    private Text fileText;
    private Button modelButton;
    private Button dataButton;
+   private Button enumButton;
    private String defaultSourceFolder;
    private String defaultPackage;
 
@@ -128,6 +129,9 @@ public class FilePage extends WizardPage
       dataButton = new Button(radioGroup, SWT.RADIO);
       dataButton.setText("Data");
 
+	  enumButton = new Button(radioGroup, SWT.RADIO);
+      enumButton.setText("Enum");
+
       // Focus
       final Text focus;
       if (defaultSourceFolder.isEmpty()) {
@@ -142,7 +146,7 @@ public class FilePage extends WizardPage
       container.getShell().getDisplay().asyncExec(() -> {
          focus.setFocus();
       });
-      
+
    }
 
    private void validate()
@@ -203,8 +207,8 @@ public class FilePage extends WizardPage
       }
 
       // Button Validation
-      if (!modelButton.getSelection() && !dataButton.getSelection()) {
-         updateStatus("Either Model or Data must be selected");
+      if (!modelButton.getSelection() && !dataButton.getSelection() && !enumButton.getSelection()) {
+         updateStatus("Either Model, Data or Enum must be selected");
          return;
       }
 
@@ -238,9 +242,18 @@ public class FilePage extends WizardPage
       return this.fileText.getText().trim();
    }
 
-   public boolean isModelSelected()
+   public String getElementType()
    {
-      return modelButton.getSelection();
+      if (modelButton.getSelection()) {
+         return "model";
+      }
+      if (dataButton.getSelection()) {
+         return "data";
+      }
+      if (enumButton.getSelection()) {
+         return "enum";
+      }
+      return null;
    }
 
    public IFile getAbsolutePath()
