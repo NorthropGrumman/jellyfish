ad5146588a28795c6c4c371c0edc2902683b7437
romellfoster <romell.foster@ngc.com>
2018-02-13 10:38:06 -0600

    edited Download Script

--- downloadNexusLifecycleReport.sh
+++ downloadNexusLifecycleReport.sh
@@ -2,6 +2,9 @@
 
 LOGFILE=$1
 OUTPUT_DIRECTORY=$2
+USERNAME=$3
+PASSWORD=$4
+
 if [ -z "$LOGFILE" ]
 then
   echo 'You must pass in the path to the Jenkins log file'
@@ -19,6 +22,6 @@ BUILD_ID=$(grep $SEARCH_STRING $LOGFILE | tail -1 | awk -F $SEARCH_STRING '{prin
 
 export no_proxy=".northgrum.com"
 
-echo "Downloading Nexus Lifecycle PDF report '$BUILD_ID'"
+echo "Downloading Nexus Lifecycle PDF report at '${SEARCH_STRING}${BUILD_ID}/pdf'"
 
-curl -L -k -u 'JustanLProvence:Passw)rd' "${SEARCH_STRING}${BUILD_ID}/pdf" > $OUTPUT_DIRECTORY/Nexus-Lifecycle-Report-$BUILD_ID.pdf
+curl -L -k -u "$USERNAME:$PASSWORD" "${SEARCH_STRING}${BUILD_ID}/pdf" > $OUTPUT_DIRECTORY/Nexus-Lifecycle-Report-$BUILD_ID.pdf
