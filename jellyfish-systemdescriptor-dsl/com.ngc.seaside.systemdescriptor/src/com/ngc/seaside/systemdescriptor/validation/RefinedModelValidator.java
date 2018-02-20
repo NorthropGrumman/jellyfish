fc623dd456698841d306667a68c9855bacbd5f78
Cameron Chaparro <cameron.chaparro@ngc.com>
2018-02-20 15:37:22 -0600

    Refactor the RefinedModelValidator class
    Add null checks to a top-level function and have that function call the
    necessary validation functions.

--- jellyfish-systemdescriptor-dsl/com.ngc.seaside.systemdescriptor/src/com/ngc/seaside/systemdescriptor/validation/RefinedModelValidator.java
+++ jellyfish-systemdescriptor-dsl/com.ngc.seaside.systemdescriptor/src/com/ngc/seaside/systemdescriptor/validation/RefinedModelValidator.java
@@ -7,15 +7,19 @@ import org.eclipse.xtext.validation.Check;
 
 public class RefinedModelValidator extends AbstractUnregisteredSystemDescriptorValidator {
     @Check
-    public void checkDoesNotParseModelThatRefinesItself(Model model) {
-        if (model.getRefinedModel().equals(model)) {
-            String msg = "A model cannot refine itself!";
-            error(msg, model, SystemDescriptorPackage.Literals.MODEL__REFINED_MODEL);
-        }
+    public void checkRefinedModel(Model model) {
+        if (model == null || model.getRefinedModel() == null)
+            return;
+
+        checkDoesNotParseModelThatRefinesUnloadableItem(model);
+        checkDoesNotParseModelThatRefinesItself(model);
+        checkDoesNotParseModelThatRedeclaresInputs(model);
+        checkDoesNotParseModelThatRedeclaresOutputs(model);
+        checkDoesNotParseModelThatRedeclaresRequires(model);
+        checkDoesNotParseModelThatRedeclaresScenarios(model);
     }
 
-    @Check
-    public void checkDoesNotParseModelThatRefinesUnloadableItem(Model model) {
+    private void checkDoesNotParseModelThatRefinesUnloadableItem(Model model) {
         // Caveat: Since we can't directly check for refinement of data types, circular dependencies,
         // etc just prevent the user from refining anything that couldn't be loaded as a model.
         if (model.getRefinedModel() != null && model.getRefinedModel().getName() == null) {
@@ -24,33 +28,36 @@ public class RefinedModelValidator extends AbstractUnregisteredSystemDescriptorV
         }
     }
 
-    @Check
-    public void checkDoesNotParseModelThatRedeclaresInputs(Model model) {
-        if (!model.getRefinedModel().getInput().getDeclarations().equals(model.getInput().getDeclarations())) {
-            String msg = "A refined model cannot re-define inputs!";
+    private void checkDoesNotParseModelThatRefinesItself(Model model) {
+        if (model.getRefinedModel().equals(model)) {
+            String msg = "A model cannot refine itself!";
             error(msg, model, SystemDescriptorPackage.Literals.MODEL__REFINED_MODEL);
         }
     }
 
-    @Check
-    public void checkDoesNotParseModelThatRedeclaresOutputs(Model model) {
-        if (!model.getRefinedModel().getOutput().getDeclarations().equals(model.getOutput().getDeclarations())) {
-            String msg = "A refined model cannot re-define outputs!";
+    private void checkDoesNotParseModelThatRedeclaresInputs(Model model) {
+        if (model.getInput() != null && model.getInput().getDeclarations() != null) {
+            String msg = "A refined model cannot add inputs!";
             error(msg, model, SystemDescriptorPackage.Literals.MODEL__REFINED_MODEL);
         }
     }
 
-    @Check
-    public void checkDoesNotParseModelThatRedeclaresRequires(Model model) {
-        if (!model.getRefinedModel().getRequires().getDeclarations().equals(model.getRequires().getDeclarations())) {
-            String msg = "A refined model cannot re-define requires!";
+    private void checkDoesNotParseModelThatRedeclaresOutputs(Model model) {
+        if (model.getOutput() != null && model.getOutput().getDeclarations() != null) {
+            String msg = "A refined model cannot add outputs!";
             error(msg, model, SystemDescriptorPackage.Literals.MODEL__REFINED_MODEL);
         }
     }
 
-    @Check
-    public void checkDoesNotParseModelThatRedeclaresScenarios(Model model) {
-        if (!model.getRefinedModel().getScenarios().equals(model.getScenarios())) {
+    private void checkDoesNotParseModelThatRedeclaresRequires(Model model) {
+        if (model.getRequires() != null && model.getRequires().getDeclarations() != null) {
+            String msg = "A refined model cannot add requirements!";
+            error(msg, model, SystemDescriptorPackage.Literals.MODEL__REFINED_MODEL);
+        }
+    }
+
+    private void checkDoesNotParseModelThatRedeclaresScenarios(Model model) {
+        if (!model.getScenarios().isEmpty()) {
             String msg = "A refined model cannot add scenarios!";
             error(msg, model, SystemDescriptorPackage.Literals.MODEL__REFINED_MODEL);
         }
