22ccab243af13121caa05d98ef391695953c84f3
matthew.lacombe@ngc.com <matthew.lacombe@ngc.com>
2018-02-28 14:01:41 +0000

    Added Java Docs to the new validators and also refined error messages

--- jellyfish-systemdescriptor-dsl/com.ngc.seaside.systemdescriptor/src/com/ngc/seaside/systemdescriptor/validation/PartsValidator.java
+++ jellyfish-systemdescriptor-dsl/com.ngc.seaside.systemdescriptor/src/com/ngc/seaside/systemdescriptor/validation/PartsValidator.java
@@ -11,12 +11,23 @@ import com.ngc.seaside.systemdescriptor.systemDescriptor.PartDeclaration;
 import com.ngc.seaside.systemdescriptor.systemDescriptor.RefinedPartDeclaration;
 import com.ngc.seaside.systemdescriptor.systemDescriptor.SystemDescriptorPackage;
 
+
+/**
+ * @author ceacide
+ *
+ *  Validates the refine Part Declaration portion of Parts  
+ */
 public class PartsValidator extends AbstractUnregisteredSystemDescriptorValidator {
 
 	@Inject
 	private IQualifiedNameProvider nameProvider;
 
 	@Check
+	/**
+	 * Entry into this validator for xtext 
+	 * 
+	 * @param part thats being validated
+	 */
 	public void checkLinkDeclaration(PartDeclaration part) {
 
 		if (part.eClass().equals(SystemDescriptorPackage.Literals.REFINED_PART_DECLARATION)) {
@@ -29,6 +40,12 @@ public class PartsValidator extends AbstractUnregisteredSystemDescriptorValidato
 
 	}
 
+	/**
+	 * Validates that the Model thats not refining another Model can't then do a refine on
+	 *  a partDeclaration 
+	 *  
+	 * @param part thats being validated 
+	 */
 	protected void checkForNonRefinedModelUsingRefinedParts(RefinedPartDeclaration part) {
 		//Bring us up to the part model
 		Model partModel = getModel(part);
@@ -42,18 +59,32 @@ public class PartsValidator extends AbstractUnregisteredSystemDescriptorValidato
 		}
 	}
 	
+	/**
+	 * Validates that the Model or somewhere in the hierarchy that this part declaration has already been 
+	 *  defined 
+	 *  
+	 * @param part thats being validated 
+	 */
 	protected void checkForRefinementOfAPartFieldThatsNotInModelBeingRefined(RefinedPartDeclaration part) {
 		Model partModel = getModel(part);
 		if (partModel != null){
 			if (!findPartName(partModel, part.getName())){
 				String msg = String.format(
-						"The requirement '%s' cannot be refined without being defined in Model '%s' thats being refined",
-						part.getName(), partModel.getName());
+						"The part '%s' cannot be refined without first being defined in Model"
+						+ " '%s' thats being refined or its hierarchy",
+						part.getName(), partModel.getRefinedModel().getName());
 				error(msg, part, SystemDescriptorPackage.Literals.FIELD_DECLARATION__NAME);	
 			}
 		}
 	}
-	
+		
+	/**
+	 * 
+	 * Grabs the container that contains the parts declaration for this requirement 
+	 * 
+	 * @param part that we want its Model container for 
+	 * @return Model that contains the refined part
+	 */
 	private Model getModel(PartDeclaration part){
 		if (part.eContainer().eContainer().eClass().equals(SystemDescriptorPackage.Literals.MODEL)) {
 			return (Model)part.eContainer().eContainer();
@@ -61,6 +92,15 @@ public class PartsValidator extends AbstractUnregisteredSystemDescriptorValidato
 		return null;
 	}
 	
+	/**
+	 * 
+	 * Looks for the part thats being refined in the Model hierarchy
+	 * 
+	 * @param model used as a starting point for the Model hierarchy 
+	 * @param partName the Name of the variable we are looking for 
+	 * @return boolean as to whether we have found the variable in the Model
+	 *    hierarchy 
+	 */
 	private boolean findPartName(Model model, String partName) {
 		boolean found = false;
 		Model parentModel = model.getRefinedModel();
