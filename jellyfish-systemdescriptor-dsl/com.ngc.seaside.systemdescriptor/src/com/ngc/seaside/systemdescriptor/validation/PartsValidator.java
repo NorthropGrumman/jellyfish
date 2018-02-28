3842ec917332678a151bfc181c224fa3adf05966
matthew.lacombe@ngc.com <matthew.lacombe@ngc.com>
2018-02-28 16:29:27 +0000

    More refactoring and cleanup

--- jellyfish-systemdescriptor-dsl/com.ngc.seaside.systemdescriptor/src/com/ngc/seaside/systemdescriptor/validation/PartsValidator.java
+++ jellyfish-systemdescriptor-dsl/com.ngc.seaside.systemdescriptor/src/com/ngc/seaside/systemdescriptor/validation/PartsValidator.java
@@ -5,11 +5,12 @@ import org.eclipse.xtext.naming.IQualifiedNameProvider;
 import org.eclipse.xtext.validation.Check;
 
 import com.google.inject.Inject;
-
+import com.ngc.seaside.systemdescriptor.systemDescriptor.FieldDeclaration;
 import com.ngc.seaside.systemdescriptor.systemDescriptor.Model;
 import com.ngc.seaside.systemdescriptor.systemDescriptor.PartDeclaration;
 import com.ngc.seaside.systemdescriptor.systemDescriptor.RefinedPartDeclaration;
 import com.ngc.seaside.systemdescriptor.systemDescriptor.SystemDescriptorPackage;
+import com.ngc.seaside.systemdescriptor.validation.util.ValidatorUtil;
 
 
 /**
@@ -31,95 +32,21 @@ public class PartsValidator extends AbstractUnregisteredSystemDescriptorValidato
 	public void checkLinkDeclaration(PartDeclaration part) {
 
 		if (part.eClass().equals(SystemDescriptorPackage.Literals.REFINED_PART_DECLARATION)) {
-			RefinedPartDeclaration refinedPart = ((RefinedPartDeclaration)part);
 			//Don't change order unless you have thought about what 
 			// should be checked first
-			checkForNonRefinedModelUsingRefinedParts(refinedPart);
-			checkForRefinementOfAPartFieldThatsNotInModelBeingRefined(refinedPart);
+			setFieldDeclarationError(ValidatorUtil.checkForNonRefinedModelUsingRefinedfields(part),
+					part);
+			setFieldDeclarationError(ValidatorUtil.checkForRefinementOfAFieldThatsNotInModelBeingRefined(part),
+					part);
 		}
 
 	}
 
-	/**
-	 * Validates that the Model thats not refining another Model can't then do a refine on
-	 *  a partDeclaration 
-	 *  
-	 * @param part thats being validated 
-	 */
-	protected void checkForNonRefinedModelUsingRefinedParts(RefinedPartDeclaration part) {
-		//Bring us up to the part model
-		Model partModel = getModel(part);
-		if (partModel !=null) {
-			if (((Model)part.eContainer().eContainer()).getRefinedModel() == null) {
-				String msg = String.format(
-						"Cannot refine part '%s' if this model '%s' is not refining another model",
-						part.getName(), partModel.getName());
-				error(msg, part, SystemDescriptorPackage.Literals.FIELD_DECLARATION__NAME);
-			}
+	private void setFieldDeclarationError(String error, FieldDeclaration requirement){
+		if (!error.isEmpty())
+		{
+			error(error, requirement, SystemDescriptorPackage.Literals.FIELD_DECLARATION__NAME);
 		}
-	}
-	
-	/**
-	 * Validates that the Model or somewhere in the hierarchy that this part declaration has already been 
-	 *  defined 
-	 *  
-	 * @param part thats being validated 
-	 */
-	protected void checkForRefinementOfAPartFieldThatsNotInModelBeingRefined(RefinedPartDeclaration part) {
-		Model partModel = getModel(part);
-		if (partModel != null){
-			if (!findPartName(partModel, part.getName())){
-				String msg = String.format(
-						"The part '%s' cannot be refined without first being defined in Model"
-						+ " '%s' thats being refined or its hierarchy",
-						part.getName(), partModel.getRefinedModel().getName());
-				error(msg, part, SystemDescriptorPackage.Literals.FIELD_DECLARATION__NAME);	
-			}
-		}
-	}
-		
-	/**
-	 * 
-	 * Grabs the container that contains the parts declaration for this requirement 
-	 * 
-	 * @param part that we want its Model container for 
-	 * @return Model that contains the refined part
-	 */
-	private Model getModel(PartDeclaration part){
-		if (part.eContainer().eContainer().eClass().equals(SystemDescriptorPackage.Literals.MODEL)) {
-			return (Model)part.eContainer().eContainer();
-		}
-		return null;
-	}
-	
-	/**
-	 * 
-	 * Looks for the part thats being refined in the Model hierarchy
-	 * 
-	 * @param model used as a starting point for the Model hierarchy 
-	 * @param partName the Name of the variable we are looking for 
-	 * @return boolean as to whether we have found the variable in the Model
-	 *    hierarchy 
-	 */
-	private boolean findPartName(Model model, String partName) {
-		boolean found = false;
-		Model parentModel = model.getRefinedModel();
-		
-		while (parentModel != null) {
-			if (parentModel.getParts() != null && 
-					parentModel.getParts().getDeclarations() != null)
-			{
-				for(PartDeclaration part : parentModel.getParts().getDeclarations()) {
-					if (part.getName().equals(partName)) {
-						found = true;
-						break;
-					}
-				}
-			}
-			parentModel = parentModel.getRefinedModel();
-		}
-		
-		return found;
 	}
 }
 
--- jellyfish-systemdescriptor-dsl/com.ngc.seaside.systemdescriptor/src/com/ngc/seaside/systemdescriptor/validation/RequiresValidator.java
+++ jellyfish-systemdescriptor-dsl/com.ngc.seaside.systemdescriptor/src/com/ngc/seaside/systemdescriptor/validation/RequiresValidator.java
@@ -4,10 +4,12 @@ import org.eclipse.xtext.naming.IQualifiedNameProvider;
 import org.eclipse.xtext.validation.Check;
 
 import com.google.inject.Inject;
+import com.ngc.seaside.systemdescriptor.systemDescriptor.FieldDeclaration;
 import com.ngc.seaside.systemdescriptor.systemDescriptor.Model;
 import com.ngc.seaside.systemdescriptor.systemDescriptor.RefinedRequireDeclaration;
 import com.ngc.seaside.systemdescriptor.systemDescriptor.RequireDeclaration;
 import com.ngc.seaside.systemdescriptor.systemDescriptor.SystemDescriptorPackage;
+import com.ngc.seaside.systemdescriptor.validation.util.ValidatorUtil;
 /**
  * 
  * @author ceacide
@@ -28,93 +30,22 @@ public class RequiresValidator extends AbstractUnregisteredSystemDescriptorValid
 	public void checkLinkDeclaration(RequireDeclaration requirement) {
 
 		if (requirement.eClass().equals(SystemDescriptorPackage.Literals.REFINED_REQUIRE_DECLARATION)) {
-			RefinedRequireDeclaration refinedRequirementPart = ((RefinedRequireDeclaration)requirement);
 			//Don't change order unless you have thought about what 
 			// should be checked first
-			checkForNonRefinedModelUsingRefinedRequires(refinedRequirementPart);
-			checkForRefinementOfARequirementFieldThatsNotInModelBeingRefined(refinedRequirementPart);
+			setFieldDeclarationError(ValidatorUtil.checkForNonRefinedModelUsingRefinedfields(requirement),
+					requirement);
+			
+			setFieldDeclarationError(
+					ValidatorUtil.checkForRefinementOfAFieldThatsNotInModelBeingRefined(requirement), 
+					requirement);
 		}
 	}
 	
-	/**
-	 * Validates that the Model thats not refining another Model can't then do a refine on
-	 *  a requireDeclaration 
-	 *  
-	 * @param requirement thats being validated 
-	 */
-	protected void checkForNonRefinedModelUsingRefinedRequires(RefinedRequireDeclaration requirement) {
-		//Bring us up to the part model
-		Model requirementModel = getModel(requirement);
-		if (requirementModel !=null) {
-			if (((Model)requirement.eContainer().eContainer()).getRefinedModel() == null) {
-				String msg = String.format(
-						"Cannot refine requirement '%s' if this model '%s' is not refining another model",
-						requirement.getName(), requirementModel.getName());
-				error(msg, requirement, SystemDescriptorPackage.Literals.FIELD_DECLARATION__NAME);
-			}
+	private void setFieldDeclarationError(String error, FieldDeclaration requirement){
+		if (!error.isEmpty())
+		{
+			error(error, requirement, SystemDescriptorPackage.Literals.FIELD_DECLARATION__NAME);
 		}
 	}
 	
-	/**
-	 * Validates that the Model or somewhere in the hierarchy that this requirement declaration 
-	 * has already been defined 
-	 *  
-	 * @param requirement thats being validated 
-	 */
-	protected void checkForRefinementOfARequirementFieldThatsNotInModelBeingRefined(RefinedRequireDeclaration requirement) {
-		Model requirementModel = getModel(requirement);
-		if (requirementModel != null){
-			if (!findRequirementName(requirementModel, requirement.getName())){
-				String msg = String.format(
-						"The requirement '%s' cannot be refined without first being defined in Model"
-						+ " '%s' thats being refined or its hierarchy",
-						requirement.getName(), requirementModel.getName());
-				error(msg, requirement, SystemDescriptorPackage.Literals.FIELD_DECLARATION__NAME);	
-			}
-		}
-	}
-	
-	/**
-	 * 
-	 * Grabs the container that contains the requirement declaration for this requirement 
-	 * 
-	 * @param requirement that we want its Model container for 
-	 * @return Model that contains the refined requirement
-	 */
-	static private Model getModel(RefinedRequireDeclaration requirement){
-		if (requirement.eContainer().eContainer().eClass().equals(SystemDescriptorPackage.Literals.MODEL)) {
-			return (Model)requirement.eContainer().eContainer();
-		}
-		return null;
-	}
-	
-	/**
-	 * 
-	 * Looks for the requirement thats being refined in the Model hierarchy
-	 * 
-	 * @param model used as a starting point for the Model hierarchy 
-	 * @param requirementName the Name of the variable we are looking for 
-	 * @return boolean as to whether we have found the variable in the Model
-	 *    hierarchy 
-	 */
-	static private boolean findRequirementName(Model model, String requirementName) {
-		boolean found = false;
-		Model parentModel = model.getRefinedModel();
-		
-		while (parentModel != null) {
-			if (parentModel.getParts() != null && 
-					parentModel.getParts().getDeclarations() != null)
-			{
-				for(RequireDeclaration requiresment : parentModel.getRequires().getDeclarations()) {
-					if (requiresment.getName().equals(requirementName)) {
-						found = true;
-						break;
-					}
-				}
-			}
-			parentModel = parentModel.getRefinedModel();
-		}
-		
-		return found;
-	}
 }
