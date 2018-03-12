bdc7914e708cef1d9136ce999332f379f4ecf769
Cameron Chaparro <cameron.chaparro@ngc.com>
2018-03-12 16:57:19 -0500

    Clean up the test

--- jellyfish-systemdescriptor/com.ngc.seaside.systemdescriptor.model.impl.xtext/src/test/java/com/ngc/seaside/systemdescriptor/model/impl/xtext/model/properties/WrappedPrimitivePropertyValueTest.java
+++ jellyfish-systemdescriptor/com.ngc.seaside.systemdescriptor.model.impl.xtext/src/test/java/com/ngc/seaside/systemdescriptor/model/impl/xtext/model/properties/WrappedPrimitivePropertyValueTest.java
@@ -7,6 +7,7 @@ import com.ngc.seaside.systemdescriptor.systemDescriptor.DblValue;
 import com.ngc.seaside.systemdescriptor.systemDescriptor.IntValue;
 import com.ngc.seaside.systemdescriptor.systemDescriptor.StringValue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -17,18 +18,24 @@ public class WrappedPrimitivePropertyValueTest extends AbstractWrappedXtextTest 
    private final static DblValue PROPERTY_FLOAT_VALUE = factory().createDblValue();
    private final static StringValue PROPERTY_STRING_VALUE = factory().createStringValue();
 
+   private WrappedPrimitivePropertyValue wrappedBooleanValue;
+   private WrappedPrimitivePropertyValue wrappedIntegerValue;
+   private WrappedPrimitivePropertyValue wrappedFloatValue;
+   private WrappedPrimitivePropertyValue wrappedStringValue;
+
+   @Before
+   public void setup() {
+      wrappedBooleanValue = new WrappedPrimitivePropertyValue(resolver(), PROPERTY_BOOLEAN_VALUE);
+      wrappedIntegerValue = new WrappedPrimitivePropertyValue(resolver(), PROPERTY_INTEGER_VALUE);
+      wrappedFloatValue = new WrappedPrimitivePropertyValue(resolver(), PROPERTY_FLOAT_VALUE);
+      wrappedStringValue = new WrappedPrimitivePropertyValue(resolver(), PROPERTY_STRING_VALUE);
+   }
+
    @Test
    public void testDoesReturnCorrectType() {
-      WrappedPrimitivePropertyValue value = new WrappedPrimitivePropertyValue(resolver(), PROPERTY_BOOLEAN_VALUE);
-      assertEquals("boolean property type is incorrect!", value.getType(), DataTypes.BOOLEAN);
-
-      value = new WrappedPrimitivePropertyValue(resolver(), PROPERTY_INTEGER_VALUE);
-      assertEquals("integer property type is incorrect!", value.getType(), DataTypes.INT);
-
-      value = new WrappedPrimitivePropertyValue(resolver(), PROPERTY_FLOAT_VALUE);
-      assertEquals("float property type is incorrect!", value.getType(), DataTypes.FLOAT);
-
-      value = new WrappedPrimitivePropertyValue(resolver(), PROPERTY_STRING_VALUE);
-      assertEquals("string property type is incorrect!", value.getType(), DataTypes.STRING);
+      assertEquals("boolean property type is incorrect!", wrappedBooleanValue.getType(), DataTypes.BOOLEAN);
+      assertEquals("integer property type is incorrect!", wrappedIntegerValue.getType(), DataTypes.INT);
+      assertEquals("float property type is incorrect!", wrappedFloatValue.getType(), DataTypes.FLOAT);
+      assertEquals("string property type is incorrect!", wrappedStringValue.getType(), DataTypes.STRING);
    }
 }
