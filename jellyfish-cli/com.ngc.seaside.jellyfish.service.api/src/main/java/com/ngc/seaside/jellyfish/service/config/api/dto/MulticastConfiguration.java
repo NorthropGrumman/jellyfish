8301a27a5527dbd6dbc5a35c05ef9fa8ef83783b
Tyler King <Tyler.King@ngc.com>
2018-03-21 14:08:55 -0500

    Add equals and hashcode to multicast configuration dto

--- jellyfish-cli/com.ngc.seaside.jellyfish.service.api/src/main/java/com/ngc/seaside/jellyfish/service/config/api/dto/MulticastConfiguration.java
+++ jellyfish-cli/com.ngc.seaside.jellyfish.service.api/src/main/java/com/ngc/seaside/jellyfish/service/config/api/dto/MulticastConfiguration.java
@@ -1,5 +1,7 @@
 package com.ngc.seaside.jellyfish.service.config.api.dto;
 
+import java.util.Objects;
+
 public class MulticastConfiguration {
 
    private String address;
@@ -21,4 +23,22 @@ public class MulticastConfiguration {
       this.port = port;
    }
 
+   @Override
+   public boolean equals(Object o) {
+      if (!(o instanceof MulticastConfiguration)) {
+         return false;
+      }
+      MulticastConfiguration that = (MulticastConfiguration) o;
+      return Objects.equals(this.address, that.address) && this.port == that.port;
+   }
+
+   @Override
+   public int hashCode() {
+      return Objects.hash(address, port);
+   }
+
+   @Override
+   public String toString() {
+      return "MulticastConfiguration[address=" + address + ", port=" + port + "]";
+   }
 }
