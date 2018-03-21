3a665b048fa1e834887ab94a4e1b2856631e4aad
Tyler King <Tyler.King@ngc.com>
2018-03-21 14:08:34 -0500

    Refactor ITransportConfigurationService to return a collection of multicast configurations

--- jellyfish-cli/com.ngc.seaside.jellyfish.service.api/src/main/java/com/ngc/seaside/jellyfish/service/config/api/ITransportConfigurationService.java
+++ jellyfish-cli/com.ngc.seaside.jellyfish.service.api/src/main/java/com/ngc/seaside/jellyfish/service/config/api/ITransportConfigurationService.java
@@ -5,17 +5,17 @@ import com.ngc.seaside.jellyfish.service.config.api.dto.MulticastConfiguration;
 import com.ngc.seaside.jellyfish.service.scenario.api.IMessagingFlow;
 import com.ngc.seaside.systemdescriptor.model.api.model.IDataReferenceField;
 
-import java.util.Optional;
+import java.util.Collection;
 
 public interface ITransportConfigurationService {
    String getTransportTopicName(IMessagingFlow flow, IDataReferenceField field);
 
    /**
-    * Returns the multicast configuration for the given field, or {@link Optional#empty()} if there is no multicast configuration for the field.
+    * Returns the multicast configurations for the given field, or an empty collection if there are no multicast configurations for the field.
     * 
     * @param options jellyfish options
     * @param field field
-    * @return the multicast configuration for the given field
+    * @return the multicast configurations for the given field
     */
-   Optional<MulticastConfiguration> getMulticastConfiguration(IJellyFishCommandOptions options, IDataReferenceField field);
+   Collection<MulticastConfiguration> getMulticastConfiguration(IJellyFishCommandOptions options, IDataReferenceField field);
 }
