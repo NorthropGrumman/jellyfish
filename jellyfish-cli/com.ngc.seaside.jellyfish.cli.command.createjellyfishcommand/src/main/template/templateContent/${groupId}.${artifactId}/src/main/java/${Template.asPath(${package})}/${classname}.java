5f6b74efc263306d1001a5b7097ea1816107f72c
Tyler King <Tyler.King@ngc.com>
2018-03-23 15:32:20 -0500

    Update create-jellyfish-command

--- jellyfish-cli/com.ngc.seaside.jellyfish.cli.command.createjellyfishcommand/src/main/template/templateContent/${groupId}.${artifactId}/src/main/java/${Template.asPath(${package})}/${classname}.java
+++ jellyfish-cli/com.ngc.seaside.jellyfish.cli.command.createjellyfishcommand/src/main/template/templateContent/${groupId}.${artifactId}/src/main/java/${Template.asPath(${package})}/${classname}.java
@@ -1,11 +1,17 @@
 package ${package};
 
 import com.ngc.blocs.service.log.api.ILogService;
+import com.ngc.seaside.jellyfish.api.CommonParameters;
 import com.ngc.seaside.jellyfish.api.DefaultParameter;
 import com.ngc.seaside.jellyfish.api.DefaultUsage;
 import com.ngc.seaside.jellyfish.api.IUsage;
 import com.ngc.seaside.jellyfish.api.IJellyFishCommand;
 import com.ngc.seaside.jellyfish.api.IJellyFishCommandOptions;
+import com.ngc.seaside.jellyfish.service.buildmgmt.api.IBuildManagementService;
+import com.ngc.seaside.jellyfish.service.template.api.ITemplateService;
+import com.ngc.seaside.jellyfish.service.name.api.IPackageNamingService;
+import com.ngc.seaside.jellyfish.service.name.api.IProjectNamingService;
+import com.ngc.seaside.jellyfish.utilities.command.AbstractJellyfishCommand;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -15,67 +21,66 @@ import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.component.annotations.ReferencePolicy;
 
 @Component(service = IJellyFishCommand.class)
-public class ${classname} implements IJellyFishCommand {
+public class ${classname} extends AbstractJellyfishCommand implements IJellyFishCommand {
 
    private static final String NAME = "${commandName}";
-   private static final IUsage USAGE = createUsage();
 
    public static final String EXAMPLE_PROPERTY = "example";
 
-   private ILogService logService;
-
-   @Override
-   public String getName() {
-      return NAME;
+   public ${classname}() {
+      super(NAME);
    }
 
    @Override
-   public IUsage getUsage() {
-      return USAGE;
-   }
-
-   @Override
-   public void run(IJellyFishCommandOptions commandOptions) {
+   protected void doRun() {
       // TODO Auto-generated method stub
    }
-   
+
    @Activate
    public void activate() {
+      super.activate();
       logService.trace(getClass(), "Activated");
    }
 
    @Deactivate
    public void deactivate() {
+      super.deactivate();
       logService.trace(getClass(), "Deactivated");
    }
 
-   /**
-    * Sets log service.
-    *
-    * @param ref the ref
-    */
    @Reference(cardinality = ReferenceCardinality.MANDATORY, policy = ReferencePolicy.STATIC, unbind = "removeLogService")
    public void setLogService(ILogService ref) {
-      this.logService = ref;
+      super.setLogService(ref);
    }
 
-   /**
-    * Remove log service.
-    */
-   public void removeLogService(ILogService ref) {
-      setLogService(null);
+   @Reference(cardinality = ReferenceCardinality.MANDATORY, policy = ReferencePolicy.STATIC, unbind = "removeBuildManagementService")
+   public void setBuildManagementService(IBuildManagementService ref) {
+      super.setBuildManagementService(ref);
    }
 
-   /**
-    * Create the usage for this command.
-    *
-    * @return the usage.
-    */
-   @SuppressWarnings("rawtypes")
-   private static IUsage createUsage() {
+   @Reference(cardinality = ReferenceCardinality.MANDATORY, policy = ReferencePolicy.STATIC, unbind = "removeTemplateService")
+   public void setTemplateService(ITemplateService ref) {
+      super.setTemplateService(ref);
+   }
+
+   @Reference(cardinality = ReferenceCardinality.MANDATORY, policy = ReferencePolicy.STATIC, unbind = "removeProjectNamingService")
+   public void setProjectNamingService(IProjectNamingService ref) {
+      super.setProjectNamingService(ref);
+   }
+
+   @Reference(cardinality = ReferenceCardinality.MANDATORY, policy = ReferencePolicy.STATIC, unbind = "removePackageNamingService")
+   public void setPackageNamingService(IPackageNamingService ref) {
+      super.setPackageNamingService(ref);
+   }
+
+   @Override
+   protected IUsage createUsage() {
       // TODO Auto-generated method stub
-      return new DefaultUsage("Description of ${commandName} command", 
-         new DefaultParameter(EXAMPLE_PROPERTY).setDescription("Description of example property").setRequired(false));
+      return new DefaultUsage("Description of ${commandName} command",
+         new DefaultParameter<>(EXAMPLE_PROPERTY).setDescription("Description of example property").setRequired(false),
+         CommonParameters.GROUP_ID,
+         CommonParameters.ARTIFACT_ID,
+         CommonParameters.MODEL.required());
    }
 
 }
--- jellyfish-cli/com.ngc.seaside.jellyfish.cli.command.createjellyfishcommand/src/main/template/templateContent/${groupId}.${artifactId}/src/test/java/${Template.asPath(${package})}/${classname}Test.java
+++ jellyfish-cli/com.ngc.seaside.jellyfish.cli.command.createjellyfishcommand/src/main/template/templateContent/${groupId}.${artifactId}/src/test/java/${Template.asPath(${package})}/${classname}Test.java
@@ -1,25 +1,48 @@
 package ${package};
 
-import com.ngc.blocs.test.impl.common.log.PrintStreamLogService;
+import com.ngc.blocs.service.log.api.ILogService;
+import com.ngc.seaside.jellyfish.cli.command.test.service.MockedBuildManagementService;
+import com.ngc.seaside.jellyfish.cli.command.test.service.MockedPackageNamingService;
+import com.ngc.seaside.jellyfish.cli.command.test.service.MockedProjectNamingService;
+import com.ngc.seaside.jellyfish.cli.command.test.service.MockedTemplateService;
 
 import org.junit.Assert;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ${classname}Test {
 
    private ${classname} cmd = new ${classname}();
 
-   private PrintStreamLogService logger = new PrintStreamLogService();
+   @Mock
+   private ILogService logService;
+
+   private MockedBuildManagementService buildManagementService = new MockedBuildManagementService();
+
+   private MockedTemplateService templateService = new MockedTemplateService();
 
    @Before
    public void setup() {
-      cmd.setLogService(logger);
+      cmd.setLogService(logService);
+      cmd.setBuildManagementService(buildManagementService);
+      cmd.setTemplateService(templateService);
+      cmd.setProjectNamingService(new MockedProjectNamingService());
+      cmd.setPackageNamingService(new MockedPackageNamingService());
+      cmd.activate();
    }
 
    @Test
    public void testCommand() {
       // TODO Auto-generated method stub
    }
-   
+
+   @After
+   public void cleanup() {
+      cmd.deactivate();
+   }
 }
