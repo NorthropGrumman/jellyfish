11c13c938eb7debd7396b5910426fa76f44e54a2
Tyler King <Tyler.King@ngc.com>
2018-03-23 14:48:09 -0500

    Update multicast configuration dto

--- jellyfish-cli/com.ngc.seaside.jellyfish.cli.command.testutils/src/main/java/com/ngc/seaside/jellyfish/cli/command/test/service/MockedTransportConfigurationService.java
+++ jellyfish-cli/com.ngc.seaside.jellyfish.cli.command.testutils/src/main/java/com/ngc/seaside/jellyfish/cli/command/test/service/MockedTransportConfigurationService.java
@@ -8,8 +8,21 @@ import com.ngc.seaside.jellyfish.service.scenario.api.IMessagingFlow;
 import com.ngc.seaside.systemdescriptor.model.api.model.IDataReferenceField;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
 
 public class MockedTransportConfigurationService implements ITransportConfigurationService {
+
+   private Map<String, Collection<MulticastConfiguration>> multicastConfigurations = new LinkedHashMap<>();
+
+   public MulticastConfiguration addMulticastConfiguration(String fieldName, String address, int port) {
+      MulticastConfiguration configuration = new MulticastConfiguration().setAddress(address)
+                                                                         .setPort(port);
+      multicastConfigurations.computeIfAbsent(fieldName, __ -> new LinkedHashSet<>()).add(configuration);
+      return configuration;
+   }
 
    @Override
    public String getTransportTopicName(IMessagingFlow flow, IDataReferenceField field) {
@@ -19,7 +32,7 @@ public class MockedTransportConfigurationService implements ITransportConfigurat
    @Override
    public Collection<MulticastConfiguration> getMulticastConfiguration(IJellyFishCommandOptions options,
             IDataReferenceField field) {
-      throw new UnsupportedOperationException("Not implemented");
+      return multicastConfigurations.getOrDefault(field.getName(), Collections.emptySet());
    }
 
    @Override
