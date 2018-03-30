b482f2e8ee6216c7f4d1162fa3750cb49debed96
ThomasJHooper <thomas.hooper@ngc.com>
2018-03-30 11:16:57 -0500

    Fix broken import in template.

--- jellyfish-cli/com.ngc.seaside.jellyfish.cli.command.createjavaservicebase/src/main/java/com/ngc/seaside/jellyfish/cli/command/createjavaservicebase/dto/BaseServiceDtoFactory.java
+++ jellyfish-cli/com.ngc.seaside.jellyfish.cli.command.createjavaservicebase/src/main/java/com/ngc/seaside/jellyfish/cli/command/createjavaservicebase/dto/BaseServiceDtoFactory.java
@@ -403,7 +403,7 @@ public class BaseServiceDtoFactory implements IBaseServiceDtoFactory {
       dto.getAbstractClass().getImports().add("com.ngc.seaside.service.correlation.api.ICorrelationTrigger");
       dto.getAbstractClass().getImports().add("com.ngc.blocs.requestmodel.api.IRequest");
       dto.getAbstractClass().getImports().add("com.ngc.blocs.requestmodel.api.Requests");
-      dto.getAbstractClass().getImports().add("com.ngc.seaside.request.api.ServiceRequest");
+      dto.getAbstractClass().getImports().add("com.ngc.seaside.service.request.api.IServiceRequest");
       dto.getAbstractClass().getImports().add(Consumer.class.getName());
       dto.getInterface().getImports().add("com.ngc.seaside.service.correlation.api.ILocalCorrelationEvent");
       dto.getAbstractClass().getImports().add("com.ngc.seaside.service.correlation.api.ILocalCorrelationEvent");
