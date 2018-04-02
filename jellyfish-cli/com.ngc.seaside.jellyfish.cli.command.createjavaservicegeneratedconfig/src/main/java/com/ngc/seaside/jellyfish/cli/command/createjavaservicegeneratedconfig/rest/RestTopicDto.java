5b0da11c7acd0e58e80706b032e6fb58e23a1cb1
Cameron Chaparro <cameron.chaparro@ngc.com>
2018-04-02 16:30:04 -0500

    Add initial rest dto classes

---
+++ jellyfish-cli/com.ngc.seaside.jellyfish.cli.command.createjavaservicegeneratedconfig/src/main/java/com/ngc/seaside/jellyfish/cli/command/createjavaservicegeneratedconfig/rest/RestTopicDto.java
@@ -0,0 +1,4 @@
+package com.ngc.seaside.jellyfish.cli.command.createjavaservicegeneratedconfig.rest;
+
+public class RestTopicDto {
+}
---
+++ jellyfish-cli/com.ngc.seaside.jellyfish.cli.command.createjavaservicegeneratedconfig/src/main/java/com/ngc/seaside/jellyfish/cli/command/createjavaservicegeneratedconfig/rest/RestTransportProviderConfigDto.java
@@ -0,0 +1,6 @@
+package com.ngc.seaside.jellyfish.cli.command.createjavaservicegeneratedconfig.rest;
+
+import com.ngc.seaside.jellyfish.cli.command.createjavaservicegeneratedconfig.dto.ITransportProviderConfigDto;
+
+public class RestTransportProviderConfigDto implements ITransportProviderConfigDto<RestDto> {
+}
