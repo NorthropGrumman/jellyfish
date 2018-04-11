a274f852b2426b26a7f17030ff26a1dd90537255
Tyler King <Tyler.King@ngc.com>
2018-04-11 11:40:41 -0500

    Update jellyfish-examples with changes to generated configs

--- jellyfish-examples/regressions/4/expectedOutput/com.ngc.seaside.threateval.trackpriorityservice/com.ngc.seaside.threateval.tps.tests/src/main/java/com/ngc/seaside/threateval/tps/tests/di/TrackPriorityServiceTestInjectorSource.java
+++ jellyfish-examples/regressions/4/expectedOutput/com.ngc.seaside.threateval.trackpriorityservice/com.ngc.seaside.threateval.tps.tests/src/main/java/com/ngc/seaside/threateval/tps/tests/di/TrackPriorityServiceTestInjectorSource.java
@@ -7,7 +7,7 @@ import com.google.inject.Stage;
 import com.ngc.blocs.guice.module.LogServiceModule;
 import com.ngc.blocs.guice.module.ResourceServiceModule;
 import com.ngc.blocs.guice.module.ThreadServiceModule;
-import com.ngc.seaside.service.transport.impl.defaultz.module.DefaultTransportServiceModule;
+import com.ngc.seaside.threateval.tps.testsconfig.TrackPriorityServiceTestConfigurationModule;
 
 import cucumber.api.guice.CucumberModules;
 import cucumber.runtime.java.guice.InjectorSource;
@@ -23,7 +23,7 @@ public class TrackPriorityServiceTestInjectorSource implements InjectorSource {
                                   new LogServiceModule(),
                                   new ResourceServiceModule(),
                                   new ThreadServiceModule(),
-                                  new DefaultTransportServiceModule(),
+                                  new TrackPriorityServiceTestConfigurationModule(),
                                   new TrackPriorityServiceTestModule());
    }
 }
