ae0dc881162f25fb46016e3532c03e75783df436
matthew.lacombe@ngc.com <matthew.lacombe@ngc.com>
2018-04-19 18:33:45 +0000

    com.ngc.seaside.jellyfish.cli.command.help

--- jellyfish-cli/com.ngc.seaside.jellyfish.cli.command.help/src/main/java/com/ngc/seaside/jellyfish/cli/command/help/ParameterFormat.java
+++ jellyfish-cli/com.ngc.seaside.jellyfish.cli.command.help/src/main/java/com/ngc/seaside/jellyfish/cli/command/help/ParameterFormat.java
@@ -1,12 +1,13 @@
 package com.ngc.seaside.jellyfish.cli.command.help;
 
-import com.ngc.seaside.jellyfish.utilities.console.api.ITableFormat;
 import com.ngc.seaside.jellyfish.api.IParameter;
+import com.ngc.seaside.jellyfish.utilities.console.api.ITableFormat;
 
 /**
  * Formatter for parameters with two columns: the name and the description.
  */
 class ParameterFormat implements ITableFormat<IParameter<?>> {
+
    private final int lineWidth;
    private final int maxNameWidth;
    private final int columnWidth;
@@ -14,8 +15,8 @@ class ParameterFormat implements ITableFormat<IParameter<?>> {
    /**
     * Constructor.
     *
-    * @param lineWidth max line width for the entire table
-    * @param columnWidth width of column separator
+    * @param lineWidth    max line width for the entire table
+    * @param columnWidth  width of column separator
     * @param maxNameWidth max length of command name
     */
    public ParameterFormat(int lineWidth, int columnWidth, int maxNameWidth) {
@@ -33,48 +34,48 @@ class ParameterFormat implements ITableFormat<IParameter<?>> {
    @Override
    public String getColumnName(int column) {
       switch (column) {
-      case 0:
-         return "Name";
-      case 1:
-         return "Description";
-      default:
-         throw new IllegalArgumentException("Invalid column: " + column);
+         case 0:
+            return "Name";
+         case 1:
+            return "Description";
+         default:
+            throw new IllegalArgumentException("Invalid column: " + column);
       }
    }
 
    @Override
    public ITableFormat.ColumnSizePolicy getColumnSizePolicy(int column) {
       switch (column) {
-      case 0:
-         return ITableFormat.ColumnSizePolicy.FIXED;
-      case 1:
-         return ITableFormat.ColumnSizePolicy.FIXED;
-      default:
-         throw new IllegalArgumentException("Invalid column: " + column);
+         case 0:
+            return ITableFormat.ColumnSizePolicy.FIXED;
+         case 1:
+            return ITableFormat.ColumnSizePolicy.FIXED;
+         default:
+            throw new IllegalArgumentException("Invalid column: " + column);
       }
    }
 
    @Override
    public int getColumnWidth(int column) {
       switch (column) {
-      case 0:
-         return maxNameWidth;
-      case 1:
-         return lineWidth - maxNameWidth - columnWidth * (getColumnCount() + 1);
-      default:
-         throw new IndexOutOfBoundsException("Invalid column: " + column);
+         case 0:
+            return maxNameWidth;
+         case 1:
+            return lineWidth - maxNameWidth - columnWidth * (getColumnCount() + 1);
+         default:
+            throw new IndexOutOfBoundsException("Invalid column: " + column);
       }
    }
 
    @Override
    public Object getColumnValue(IParameter<?> object, int column) {
       switch (column) {
-      case 0:
-         return object.getName();
-      case 1:
-         return object.getDescription();
-      default:
-         throw new IndexOutOfBoundsException("Invalid column: " + column);
+         case 0:
+            return object.getName();
+         case 1:
+            return object.getDescription();
+         default:
+            throw new IndexOutOfBoundsException("Invalid column: " + column);
       }
    }
 
