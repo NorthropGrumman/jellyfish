c6020374d8bf1f7b582f0041e8dd8671b043ac1e
matthew.lacombe@ngc.com <matthew.lacombe@ngc.com>
2018-04-23 17:26:55 +0000

    com.ngc.seaside.jellyfish.service.config.impl.transportconfigurationservice

--- jellyfish-cli/com.ngc.seaside.jellyfish.service.config.impl.transportconfigurationservice/src/main/java/com/ngc/seaside/jellyfish/service/config/impl/transportconfigurationservice/CommonConfigurationUtils.java
+++ jellyfish-cli/com.ngc.seaside.jellyfish.service.config.impl.transportconfigurationservice/src/main/java/com/ngc/seaside/jellyfish/service/config/impl/transportconfigurationservice/CommonConfigurationUtils.java
@@ -8,19 +8,23 @@ import com.ngc.seaside.systemdescriptor.model.api.model.properties.IPropertyData
 import java.util.Objects;
 
 public class CommonConfigurationUtils {
-   static final String NETWORK_INTERFACE_QUALIFIED_NAME = "com.ngc.seaside.systemdescriptor.deployment.NetworkInterface";
+
+   static final String
+         NETWORK_INTERFACE_QUALIFIED_NAME =
+         "com.ngc.seaside.systemdescriptor.deployment.NetworkInterface";
    static final String NETWORK_ADDRESS_QUALIFIED_NAME = "com.ngc.seaside.systemdescriptor.deployment.NetworkAddress";
    static final String NETWORK_INTERFACE_NAME_FIELD_NAME = "name";
    static final String NETWORK_ADDRESS_ADDRESS_FIELD_NAME = "address";
-   
+
    static NetworkInterface getNetworkInterface(IPropertyDataValue value) {
       if (!Objects.equals(NETWORK_INTERFACE_QUALIFIED_NAME, value.getReferencedDataType().getFullyQualifiedName())) {
-         throw new IllegalArgumentException("Property " + value + " is not of type " + NETWORK_INTERFACE_QUALIFIED_NAME);
+         throw new IllegalArgumentException(
+               "Property " + value + " is not of type " + NETWORK_INTERFACE_QUALIFIED_NAME);
       }
       String name = value.getPrimitive(getField(value, NETWORK_INTERFACE_NAME_FIELD_NAME)).getString();
       return new NetworkInterface(name);
    }
-   
+
    static NetworkAddress getNetworkAddress(IPropertyDataValue value) {
       if (!Objects.equals(NETWORK_ADDRESS_QUALIFIED_NAME, value.getReferencedDataType().getFullyQualifiedName())) {
          throw new IllegalArgumentException("Property " + value + " is not of type " + NETWORK_ADDRESS_QUALIFIED_NAME);
@@ -28,10 +32,10 @@ public class CommonConfigurationUtils {
       String address = value.getPrimitive(getField(value, NETWORK_ADDRESS_ADDRESS_FIELD_NAME)).getString();
       return new NetworkAddress().setAddress(address);
    }
-   
+
    static IDataField getField(IPropertyDataValue value, String fieldName) {
       return value.getFieldByName(fieldName)
-                  .orElseThrow(() -> new IllegalStateException("Missing " + fieldName + " field"));
+            .orElseThrow(() -> new IllegalStateException("Missing " + fieldName + " field"));
    }
-   
+
 }
