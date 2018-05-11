8042bee1acf1d45153be77a508d84f97a96c17bc
ThomasJHooper <thomas.hooper@ngc.com>
2018-05-11 15:10:48 -0500

    Jellyfish generates projects with v2.11.0 of gradle plugins.

--- jellyfish-cli/com.ngc.seaside.jellyfish.service.buildmgmt.impl.buildmgmtservice/src/main/java/com/ngc/seaside/jellyfish/service/buildmgmt/impl/buildmgmtservice/DefaultDependenciesConfiguration.java
+++ jellyfish-cli/com.ngc.seaside.jellyfish.service.buildmgmt.impl.buildmgmtservice/src/main/java/com/ngc/seaside/jellyfish/service/buildmgmt/impl/buildmgmtservice/DefaultDependenciesConfiguration.java
@@ -38,7 +38,7 @@ public class DefaultDependenciesConfiguration {
    private static void configureSeasideGradlePlugins(DependenciesConfiguration config) {
       config.addGroup()
             .versionPropertyName("seasidePluginsVersion")
-            .version("2.11.0-SNAPSHOT")
+            .version("2.11.0")
             .includes(artifact("gradle.plugins")
                             .groupId("com.ngc.seaside")
                             .scope(DependencyScope.BUILDSCRIPT));
