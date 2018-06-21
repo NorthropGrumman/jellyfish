eee9443fbf9dfe906ea6e3f208877c7a28125f04
Tyler King <Tyler.King@ngc.com>
2018-06-21 09:08:08 -0500

    Update jellyfish-examples

--- jellyfish-cli-commands/com.ngc.seaside.jellyfish.service.buildmgmt.impl.buildmgmtservice/src/main/java/com/ngc/seaside/jellyfish/service/buildmgmt/impl/buildmgmtservice/DefaultDependenciesConfiguration.java
+++ jellyfish-cli-commands/com.ngc.seaside.jellyfish.service.buildmgmt.impl.buildmgmtservice/src/main/java/com/ngc/seaside/jellyfish/service/buildmgmt/impl/buildmgmtservice/DefaultDependenciesConfiguration.java
@@ -48,7 +48,7 @@ public class DefaultDependenciesConfiguration {
    private static void configureStarfish(DependenciesConfiguration config) {
       config.addGroup()
             .versionPropertyName("starfishVersion")
-            .version("2.8.1")
+            .version("2.10.0-SNAPSHOT")
             .defaultGroupId("com.ngc.seaside")
             .defaultScope(DependencyScope.BUILD)
             .includes(artifact("service.api"),
