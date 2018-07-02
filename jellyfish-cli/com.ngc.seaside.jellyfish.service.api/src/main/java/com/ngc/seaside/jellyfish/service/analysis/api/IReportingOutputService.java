3664863af04a5f2da3efb346a78f92bc0da01bdc
ThomasJHooper <thomas.hooper@ngc.com>
2018-07-02 13:34:06 -0500

    Update versions for flex-markdown.

--- jellyfish-cli/com.ngc.seaside.jellyfish.service.api/src/main/java/com/ngc/seaside/jellyfish/service/analysis/api/IReportingOutputService.java
+++ jellyfish-cli/com.ngc.seaside.jellyfish.service.api/src/main/java/com/ngc/seaside/jellyfish/service/analysis/api/IReportingOutputService.java
@@ -1,7 +1,23 @@
 package com.ngc.seaside.jellyfish.service.analysis.api;
 
+import com.ngc.seaside.systemdescriptor.service.source.api.ISourceLocation;
+
 public interface IReportingOutputService {
 
+   /**
+    * Converts a raw source message to some output suitable for reporting.
+    *
+    * @param message the raw source message to convert
+    * @return the reporting output
+    */
+   String convert(String message);
 
+   /**
+    * Converts the given source location to some output suitable for reporting.
+    *
+    * @param sourceLocation the source location
+    * @return the reporting output
+    */
+   String convert(ISourceLocation sourceLocation);
 
 }
