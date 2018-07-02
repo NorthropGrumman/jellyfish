4ae167b834776542241a0cd92ab1192a9368b9b2
ThomasJHooper <thomas.hooper@ngc.com>
2018-07-02 13:33:41 -0500

    Add html report service.

---
+++ jellyfish-cli-analysis-commands/com.ngc.seaside.jellyfish.cli.command.htmlanalysisreport/src/main/java/com/ngc/seaside/jellyfish/cli/command/report/html/HtmlAnalysisReportCommandGuiceWrapper.java
@@ -0,0 +1,46 @@
+package com.ngc.seaside.jellyfish.cli.command.report.html;
+
+import com.google.inject.Inject;
+
+import com.ngc.blocs.service.log.api.ILogService;
+import com.ngc.seaside.jellyfish.api.ICommand;
+import com.ngc.seaside.jellyfish.api.ICommandOptions;
+import com.ngc.seaside.jellyfish.api.IUsage;
+import com.ngc.seaside.jellyfish.service.analysis.api.IAnalysisService;
+import com.ngc.seaside.jellyfish.service.analysis.api.IReportingOutputService;
+
+/**
+ * The Guice wrapper for the HTML command.
+ */
+public class HtmlAnalysisReportCommandGuiceWrapper implements ICommand<ICommandOptions> {
+
+   private final HtmlAnalysisReportCommand delegate = new HtmlAnalysisReportCommand();
+
+   /**
+    * Creates a new command wrapper.
+    */
+   @Inject
+   public HtmlAnalysisReportCommandGuiceWrapper(ILogService logService,
+                                                IAnalysisService analysisService,
+                                                IReportingOutputService reportingOutputService) {
+      delegate.setLogService(logService);
+      delegate.setAnalysisService(analysisService);
+      delegate.setReportingOutputService(reportingOutputService);
+      delegate.activate();
+   }
+
+   @Override
+   public String getName() {
+      return delegate.getName();
+   }
+
+   @Override
+   public IUsage getUsage() {
+      return delegate.getUsage();
+   }
+
+   @Override
+   public void run(ICommandOptions options) {
+      delegate.run(options);
+   }
+}
