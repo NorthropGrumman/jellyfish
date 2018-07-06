d2dd2c099c2db71ded923fbcbeca83bb82fe8bdb
ThomasJHooper <thomas.hooper@ngc.com>
2018-07-06 07:29:13 -0500

    Configure JF to use starfish v2.12.0."

--- jellyfish-cli-commands/com.ngc.seaside.jellyfish.service.buildmgmt.impl.buildmgmtservice/src/main/java/com/ngc/seaside/jellyfish/service/buildmgmt/impl/buildmgmtservice/DefaultDependenciesConfiguration.java
+++ jellyfish-cli-commands/com.ngc.seaside.jellyfish.service.buildmgmt.impl.buildmgmtservice/src/main/java/com/ngc/seaside/jellyfish/service/buildmgmt/impl/buildmgmtservice/DefaultDependenciesConfiguration.java
@@ -48,7 +48,7 @@ public class DefaultDependenciesConfiguration {
    private static void configureStarfish(DependenciesConfiguration config) {
       config.addGroup()
             .versionPropertyName("starfishVersion")
-            .version("2.11.1")
+            .version("2.12.0")
             .defaultGroupId("com.ngc.seaside")
             .defaultScope(DependencyScope.BUILD)
             .includes(artifact("service.api"),
