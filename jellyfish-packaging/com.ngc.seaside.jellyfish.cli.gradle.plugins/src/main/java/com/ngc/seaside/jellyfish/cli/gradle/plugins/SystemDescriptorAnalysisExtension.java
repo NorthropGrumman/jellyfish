21c54c8a84a5a285b5b638a2e83ac8973ee78b7e
Tyler King <Tyler.King@ngc.com>
2018-07-31 10:54:22 -0500

    Add analysis extension to system descriptor project plugin

---
+++ jellyfish-packaging/com.ngc.seaside.jellyfish.cli.gradle.plugins/src/main/java/com/ngc/seaside/jellyfish/cli/gradle/plugins/SystemDescriptorAnalysisExtension.java
@@ -0,0 +1,106 @@
+package com.ngc.seaside.jellyfish.cli.gradle.plugins;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Extension for running analysis commands for the system descriptor project, such as through Sonarqube.
+ */
+public class SystemDescriptorAnalysisExtension {
+
+   public static final String EXTENSION_NAME = "sdAnalysis";
+
+   private final Set<String> commands = new LinkedHashSet<>();
+   private final Map<String, Object> args = new LinkedHashMap<>();
+
+   /**
+    * Sets the analysis commands to be run.
+    * 
+    * @param commands analysis commands
+    */
+   public void setCommands(Iterable<Object> commands) {
+      this.commands.clear();
+      commands(commands);
+   }
+
+   /**
+    * Returns the set of analysis commands to be run.
+    * 
+    * @return the set of analysis commands
+    */
+   public Set<String> getCommands() {
+      return commands;
+   }
+
+   /**
+    * Adds the given analysis command to be run.
+    * 
+    * @param command analysis command
+    */
+   public void command(Object command) {
+      commands(command);
+   }
+
+   /**
+    * Adds the given analysis commands to be run.
+    * 
+    * @param commands analysis commands
+    */
+   public void commands(Object... commands) {
+      commands(Arrays.asList(commands));
+   }
+
+   /**
+    * Adds the given analysis commands to be run.
+    * 
+    * @param commands analysis commands
+    */
+   public void commands(Iterable<Object> commands) {
+      for (Object command : commands) {
+         this.commands.add(command.toString());
+      }
+   }
+
+   /**
+    * Returns the arguments to be used for the analysis.
+    * 
+    * @return the arguments to be used for the analysis
+    */
+   public Map<String, Object> getArgs() {
+      return args;
+   }
+
+   /**
+    * Sets the arguments to be used for the analysis.
+    * 
+    * @param args the arguments to be used for the analysis
+    */
+   public void setArgs(Map<String, Object> args) {
+      args.clear();
+      args(args);
+   }
+
+   /**
+    * Adds an argument to be used for analysis.
+    * 
+    * @param key key
+    * @param value value
+    */
+   public void arg(String key, Object value) {
+      args(Collections.singletonMap(key, value));
+   }
+
+   /**
+    * Adds the arguments to be used for the analysis.
+    * 
+    * @param args the arguments to be used for the analysis
+    */
+   public void args(Map<String, Object> args) {
+      this.args.putAll(args);
+   }
+
+}
