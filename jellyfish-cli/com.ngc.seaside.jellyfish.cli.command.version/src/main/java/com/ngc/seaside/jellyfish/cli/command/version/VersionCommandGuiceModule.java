afa91f80712c8f91fa9f207f0682e2f765175793
Cameron Chaparro <cameron.chaparro@ngc.com>
2018-09-05 17:13:43 -0500

    Add implementations of Guice wrapper and module

--- jellyfish-cli/com.ngc.seaside.jellyfish.cli.command.version/src/main/java/com/ngc/seaside/jellyfish/cli/command/version/VersionCommandGuiceModule.java
+++ jellyfish-cli/com.ngc.seaside.jellyfish.cli.command.version/src/main/java/com/ngc/seaside/jellyfish/cli/command/version/VersionCommandGuiceModule.java
@@ -1,5 +1,16 @@
 package com.ngc.seaside.jellyfish.cli.command.version;
 
-public class VersionCommandGuiceModule {
+import com.google.inject.AbstractModule;
+import com.google.inject.multibindings.Multibinder;
 
+import com.ngc.seaside.jellyfish.api.ICommand;
+
+public class VersionCommandGuiceModule extends AbstractModule {
+
+   @Override
+   protected void configure() {
+      Multibinder.newSetBinder(binder(), ICommand.class)
+            .addBinding()
+            .to(VersionCommandGuiceWrapper.class);
+   }
 }
--- jellyfish-cli/com.ngc.seaside.jellyfish.cli.command.version/src/main/java/com/ngc/seaside/jellyfish/cli/command/version/VersionCommandGuiceWrapper.java
+++ jellyfish-cli/com.ngc.seaside.jellyfish.cli.command.version/src/main/java/com/ngc/seaside/jellyfish/cli/command/version/VersionCommandGuiceWrapper.java
@@ -1,5 +1,33 @@
 package com.ngc.seaside.jellyfish.cli.command.version;
 
-public class VersionCommandGuiceWrapper {
+import com.google.inject.Inject;
 
+import com.ngc.blocs.service.log.api.ILogService;
+import com.ngc.seaside.jellyfish.api.ICommand;
+import com.ngc.seaside.jellyfish.api.ICommandOptions;
+import com.ngc.seaside.jellyfish.api.IUsage;
+
+public class VersionCommandGuiceWrapper implements ICommand<ICommandOptions> {
+
+   private final VersionCommand delegate = new VersionCommand();
+
+   @Inject
+   public VersionCommandGuiceWrapper(ILogService logService) {
+      delegate.setLogService(logService);
+   }
+
+   @Override
+   public String getName() {
+      return delegate.getName();
+   }
+
+   @Override
+   public IUsage getUsage() {
+      return delegate.getUsage();
+   }
+
+   @Override
+   public void run(ICommandOptions commandOptions) {
+      delegate.run(commandOptions);
+   }
 }
